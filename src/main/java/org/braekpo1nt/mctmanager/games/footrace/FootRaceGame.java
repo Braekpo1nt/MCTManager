07eb7f904199d2bbfd2e1ffbf5f94c7e4c4359fb
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-03-10 14:19:30 -0500

    $1

--- src/main/java/org/braekpo1nt/mctmanager/games/footrace/FootRaceGame.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/footrace/FootRaceGame.java
@@ -23,12 +23,18 @@ public class FootRaceGame implements Listener {
      * Holds the Foot Race world
      */
     private final World footRaceWorld;
+    private final BoundingBox finishLine = new BoundingBox(2396, 80, 295, 2404, 79, 308);
+    private final List<Player> participants;
     private final Map<Player, Long> lapCooldowns;
+    private final Map<Player, Integer> laps;
     
     public FootRaceGame(Main plugin, List<Player> participants) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        this.participants = participants;
         
-        lapCooldowns = participants.stream().collect(Collectors.toMap(participant -> participant, key -> System.currentTimeMillis()));
+        lapCooldowns = participants.stream().collect(
+                Collectors.toMap(participant -> participant, key -> System.currentTimeMillis()));
+        laps = participants.stream().collect(Collectors.toMap(participant -> participant, key -> 0));
         
         Plugin multiversePlugin = Bukkit.getPluginManager().getPlugin("Multiverse-Core");
         MultiverseCore multiverseCore = ((MultiverseCore) multiversePlugin);
@@ -44,25 +50,29 @@ public class FootRaceGame implements Listener {
     public void onPlayerCrossFinishLine(PlayerMoveEvent event) {
         if (gameActive) {
             Player player = event.getPlayer();
-            if (player.getWorld().equals(footRaceWorld)) {
-                BoundingBox finishLine = new BoundingBox(2396, 80, 295, 2404, 79, 308);
-                if (finishLine.contains(player.getLocation().toVector())) {
-                    
-                    // if this player is a participant
-                    if (lapCooldowns.containsKey(player)) {
-                        long lastMoveTime = lapCooldowns.get(player);
-                        long currentTime = System.currentTimeMillis();
-                        long coolDownTime = 3000L; // 3 second
-                        if (currentTime - lastMoveTime < coolDownTime) {
-                            //Not enough time has elapsed, return without doing anything
-                            return;
-                        }
-                    }
-                    lapCooldowns.put(player, System.currentTimeMillis());
-
-                    player.sendMessage("You crossed the finish line!");
-                    
+            if (!participants.contains(player)) {
+                return;
+            }
+            if (!player.getWorld().equals(footRaceWorld)) {
+                return;
+            }
+            if (finishLine.contains(player.getLocation().toVector())) {
+                long lastMoveTime = lapCooldowns.get(player);
+                long currentTime = System.currentTimeMillis();
+                long coolDownTime = 3000L; // 3 second
+                if (currentTime - lastMoveTime < coolDownTime) {
+                    //Not enough time has elapsed, return without doing anything
+                    return;
                 }
+                lapCooldowns.put(player, System.currentTimeMillis());
+                int lastLap = laps.get(player);
+                int currentLap = lastLap + 1;
+                if (currentLap < 3) {
+                    laps.put(player, currentLap);
+                    player.sendMessage("Lap " + currentLap);
+                    return;
+                }
+                player.sendMessage("You finished all 3 laps!");
             }
         }
     }
