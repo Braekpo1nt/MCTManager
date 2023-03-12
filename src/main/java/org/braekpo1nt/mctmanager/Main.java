d1475c43c25b7122a9089c20b7fbc728ebcf5f3b
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-03-11 19:45:25 -0500

    $1

--- src/main/java/org/braekpo1nt/mctmanager/Main.java
+++ src/main/java/org/braekpo1nt/mctmanager/Main.java
@@ -1,8 +1,8 @@
 package org.braekpo1nt.mctmanager;
 
+import org.braekpo1nt.mctmanager.commands.MCTCommand;
 import org.braekpo1nt.mctmanager.commands.MCTDebugCommand;
 import org.braekpo1nt.mctmanager.commands.MCTMVTestCommand;
-import org.braekpo1nt.mctmanager.commands.MCTStartGameCommand;
 import org.braekpo1nt.mctmanager.games.GameManager;
 import org.braekpo1nt.mctmanager.listeners.HubBoundaryListener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,7 +15,7 @@ public final class Main extends JavaPlugin {
         
         // Commands
         new MCTDebugCommand(this);
-        new MCTStartGameCommand(this, gameManager);
+        new MCTCommand(this, gameManager);
         new MCTMVTestCommand(this);
         
         // Listeners
