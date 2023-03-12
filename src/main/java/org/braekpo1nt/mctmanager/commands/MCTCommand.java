54b83303bb8eec9842f7739125d99f90285a4e08
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-03-11 19:44:59 -0500

    added mct main command

---
+++ src/main/java/org/braekpo1nt/mctmanager/commands/MCTCommand.java
@@ -0,0 +1,55 @@
+package org.braekpo1nt.mctmanager.commands;
+
+import org.braekpo1nt.mctmanager.Main;
+import org.braekpo1nt.mctmanager.games.GameManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+
+public class MCTCommand implements TabExecutor {
+
+    Map<String, CommandExecutor> subCommands = new HashMap<>();
+    
+    public MCTCommand(Main plugin, GameManager gameManager) {
+        plugin.getCommand("mct").setExecutor(this);
+        subCommands.put("startgame", new StartGameCommand(gameManager));
+    }
+    
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage("Usage: /mct <option>");
+            return false;
+        }
+        if (!subCommands.containsKey(args[0])) {
+            sender.sendMessage(String.format("Argument %s is not recognized.", args[0]));
+        }
+        
+        return subCommands.get(args[0]).onCommand(sender, command, label, Arrays.copyOfRange(args, 1, args.length));
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length == 1) {
+            List<String> subCommandNames = subCommands.keySet().stream().toList();
+            subCommandNames.sort(Comparator.naturalOrder());
+            return subCommandNames;
+        }
+        if (args.length > 1) {
+            if (!subCommands.containsKey(args[0])) {
+                return null;
+            }
+            CommandExecutor subCommand = subCommands.get(args[0]);
+            if (subCommands instanceof TabExecutor) {
+                TabExecutor subTabCommand = ((TabExecutor) subCommand);
+                return subTabCommand.onTabComplete(sender, command, label, Arrays.copyOfRange(args, 1, args.length));
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/org/braekpo1nt/mctmanager/commands/StartGameCommand.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/StartGameCommand.java
@@ -3,6 +3,7 @@ package org.braekpo1nt.mctmanager.commands;
 import org.braekpo1nt.mctmanager.Main;
 import org.braekpo1nt.mctmanager.games.GameManager;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
@@ -10,19 +11,25 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class MCTStartGameCommand implements TabExecutor {
+public class MCTCommand implements TabExecutor {
     
     private final GameManager gameManager;
+    Map<String, CommandExecutor> subCommands = new HashMap<>();
     
-    public MCTStartGameCommand(Main plugin, GameManager gameManager) {
+    public MCTCommand(Main plugin, GameManager gameManager) {
         this.gameManager = gameManager;
         plugin.getCommand("mctstartgame").setExecutor(this);
     }
     
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage("Usage: /mct <option>");
+        }
         if (args.length == 1 && sender instanceof Player) {
             gameManager.startGame(args[0], ((Player) sender));
         }
