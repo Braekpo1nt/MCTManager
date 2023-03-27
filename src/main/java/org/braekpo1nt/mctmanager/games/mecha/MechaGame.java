e6e2d4d5748a5021928033d62dbd307a05eedfd7
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-03-27 17:54:40 -0400

    added teamlocations to mecha

--- src/main/java/org/braekpo1nt/mctmanager/games/mecha/MechaGame.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/mecha/MechaGame.java
@@ -1,18 +1,25 @@
 package org.braekpo1nt.mctmanager.games.mecha;
 
+import com.onarandombox.MultiverseCore.utils.AnchorManager;
+import fr.mrmicky.fastboard.FastBoard;
 import org.braekpo1nt.mctmanager.Main;
 import org.braekpo1nt.mctmanager.games.GameManager;
 import org.braekpo1nt.mctmanager.games.MCTGame;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MechaGame implements MCTGame {
     
     private final Main plugin;
     private final GameManager gameManager;
-
+    private List<Player> participants;
+    
     public MechaGame(Main plugin, GameManager gameManager) {
         this.plugin = plugin;
         this.gameManager = gameManager;
@@ -21,11 +28,34 @@ public class MechaGame implements MCTGame {
 
     @Override
     public void start(List<Player> participants) {
+        this.participants = participants;
         Bukkit.getLogger().info("Started mecha");
     }
 
     @Override
     public void stop() {
         Bukkit.getLogger().info("Stopped mecha");
+    }
+    
+    private void initializeFastboards() {
+        for (Player participant : participants) {
+            FastBoard board = new FastBoard(participant);
+            board.updateTitle(ChatColor.RED+"MECHA");
+            
+        }
+    }
+    
+    private void teleportPlayersToStartingPositions() {
+        AnchorManager anchorManager = Main.multiverseCore.getAnchorManager();
+        Map<String, Location> teamLocations = new HashMap<>();
+        teamLocations.put("orange", anchorManager.getAnchorLocation("mecha-orange"));
+        teamLocations.put("yellow", anchorManager.getAnchorLocation("mecha-yellow"));
+        teamLocations.put("green", anchorManager.getAnchorLocation("mecha-green"));
+        teamLocations.put("dark-green", anchorManager.getAnchorLocation("mecha-dark-green"));
+        teamLocations.put("cyan", anchorManager.getAnchorLocation("mecha-cyan"));
+        teamLocations.put("blue", anchorManager.getAnchorLocation("mecha-blue"));
+        teamLocations.put("purple", anchorManager.getAnchorLocation("mecha-purple"));
+        teamLocations.put("red", anchorManager.getAnchorLocation("mecha-red"));
+        
     }
 }
