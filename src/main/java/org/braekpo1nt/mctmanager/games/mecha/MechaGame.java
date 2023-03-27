e424e54ed43d2c9726c1c4d401dd5f3ccb5e5626
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-03-27 19:59:00 -0400

    added boards to MECHA

--- src/main/java/org/braekpo1nt/mctmanager/games/mecha/MechaGame.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/mecha/MechaGame.java
@@ -2,6 +2,7 @@ package org.braekpo1nt.mctmanager.games.mecha;
 
 import com.onarandombox.MultiverseCore.utils.AnchorManager;
 import fr.mrmicky.fastboard.FastBoard;
+import it.unimi.dsi.fastutil.Function;
 import org.braekpo1nt.mctmanager.Main;
 import org.braekpo1nt.mctmanager.games.GameManager;
 import org.braekpo1nt.mctmanager.games.MCTGame;
@@ -13,12 +14,14 @@ import org.bukkit.entity.Player;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class MechaGame implements MCTGame {
     
     private final Main plugin;
     private final GameManager gameManager;
     private List<Player> participants;
+    private Map<UUID, FastBoard> boards;
     
     public MechaGame(Main plugin, GameManager gameManager) {
         this.plugin = plugin;
@@ -29,19 +32,35 @@ public class MechaGame implements MCTGame {
     @Override
     public void start(List<Player> participants) {
         this.participants = participants;
+        initializeFastboards();
         Bukkit.getLogger().info("Started mecha");
     }
 
     @Override
     public void stop() {
+        hideFastBoards();
         Bukkit.getLogger().info("Stopped mecha");
     }
     
     private void initializeFastboards() {
         for (Player participant : participants) {
             FastBoard board = new FastBoard(participant);
-            board.updateTitle(ChatColor.RED+"MECHA");
-            
+            board.updateTitle(ChatColor.BLUE+"MECHA");
+            board.updateLines(
+                    ChatColor.RED+"Kills: 0",
+                    "",
+                    ChatColor.DARK_PURPLE+"Boarder: 00:00",
+                    ""
+            );
+            boards.put(participant.getUniqueId(), board);
+        }
+    }
+    
+    private void hideFastBoards() {
+        for (FastBoard board : boards.values()) {
+            if (!board.isDeleted()) {
+                board.delete();
+            }
         }
     }
     
@@ -56,6 +75,10 @@ public class MechaGame implements MCTGame {
         teamLocations.put("blue", anchorManager.getAnchorLocation("mecha-blue"));
         teamLocations.put("purple", anchorManager.getAnchorLocation("mecha-purple"));
         teamLocations.put("red", anchorManager.getAnchorLocation("mecha-red"));
-        
+        for (Player participant : participants) {
+            String team = gameManager.getTeamName(participant.getUniqueId());
+            Location teamLocation = teamLocations.get(team);
+            participant.teleport(teamLocation);
+        }
     }
 }
