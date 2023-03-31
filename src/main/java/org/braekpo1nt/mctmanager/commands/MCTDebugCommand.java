c498785a12ef561f611c68423581b3df18270f40
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-03-31 14:12:51 -0400

    added robust example of quoted string in MCTDebug

--- src/main/java/org/braekpo1nt/mctmanager/commands/MCTDebugCommand.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/MCTDebugCommand.java
@@ -1,27 +1,15 @@
 package org.braekpo1nt.mctmanager.commands;
 
 import com.onarandombox.MultiverseCore.api.MVWorldManager;
-import com.onarandombox.MultiverseCore.api.MultiverseWorld;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.title.Title;
 import org.braekpo1nt.mctmanager.Main;
 import org.bukkit.*;
-import org.bukkit.block.Block;
-import org.bukkit.block.Chest;
-import org.bukkit.block.structure.Mirror;
-import org.bukkit.block.structure.StructureRotation;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.loot.LootTable;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.structure.Structure;
 import org.jetbrains.annotations.NotNull;
 
-import java.time.Duration;
-import java.util.Random;
+import java.util.Arrays;
 
 /**
  * A utility command for testing various things, so I don't have to create a new command. 
@@ -46,26 +34,53 @@ public class MCTDebugCommand implements CommandExecutor {
             sender.sendMessage("This command can only be run by a player.");
             return true;
         }
+    
+        int startIndex = -1;
+        int endIndex = -1;
+    
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("\"") && startIndex == -1) {
+                startIndex = i;
+                continue;
+            }
+        
+            if (args[i].endsWith("\"") && endIndex == -1) {
+                endIndex = i;
+            }
+        }
+    
+        if (startIndex == -1 || endIndex == -1 || startIndex > endIndex) {
+            sender.sendMessage("Usage: /mctdebug [args] <\"message text\"> [more args]");
+            return false;
+        }
+    
+        StringBuilder beforeBuilder = new StringBuilder();
+        for (int i = 0; i < startIndex; i++) {
+            beforeBuilder.append(args[i]);
+            beforeBuilder.append(" ");
+        }
+    
+        StringBuilder messageBuilder = new StringBuilder();
+        for (int i = startIndex; i <= endIndex; i++) {
+            messageBuilder.append(args[i]);
+            messageBuilder.append(" ");
+        }
+    
+        StringBuilder afterBuilder = new StringBuilder();
+        for (int i = endIndex + 1; i < args.length; i++) {
+            afterBuilder.append(args[i]);
+            afterBuilder.append(" ");
+        }
+        
+        sender.sendMessage("Before substring: " + beforeBuilder.toString().trim());
+        String messageWithQuotes = messageBuilder.toString().trim();
+        String message = messageWithQuotes.substring(1, messageWithQuotes.length() - 1).trim();
+        sender.sendMessage("Substring: " + message);
+        sender.sendMessage("After substring: " + afterBuilder.toString().trim());
+    
+        return true;
+        
 //        Player player = ((Player) sender).getPlayer();
-        if (args.length < 1) {
-            sender.sendMessage("usage: /mctdebug [on|off]");
-            return true;
-        }
-        
-        switch (args[0]) {
-            case "on":
-                worldBorder.setCenter(0, 0);
-                worldBorder.setSize(248);
-                kickOffBoarderShrinking();
-                return true;
-            case "off":
-                Bukkit.getScheduler().cancelTask(boarderShrinkingTaskId);
-                worldBorder.reset();
-                return true;
-            default:
-                sender.sendMessage("Not recognized option: " + args[0]);
-        }
-        
 //        Component mainTitle = Component.text("Main title");
 //        Component subTitle = Component.text("Subtitle");
 //
@@ -73,58 +88,10 @@ public class MCTDebugCommand implements CommandExecutor {
 //        Title title = Title.title(mainTitle, subTitle, times);
 //        sender.showTitle(title);
         
-        return true;
     }
     
-    private void kickOffBoarderShrinking() {
-//        int[] sizes = new int[]{180, 150, 100, 50, 25, 2};
-//        int[] delays = new int[]{90, 70, 60, 80, 60, 30};
-//        int[] durations = new int[]{25,0, 100, 50, 25, 2};
-        int[] sizes = new int[]{180, 150, 100};
-        int[] delays = new int[]{11, 10, 9};
-        int[] durations = new int[]{15, 10, 5};
-        this.boarderShrinkingTaskId = new BukkitRunnable() {
-            int delay = 0;
-            int duration = 0;
-            boolean onDelay = false;
-            boolean onDuration = false;
-            int sceneIndex = 0;
-            @Override
-            public void run() {
-                if (onDelay) {
-                    Bukkit.getLogger().info(String.format("Delaying %d/%d", delay, delays[sceneIndex]));
-                    if (delay <= 0) {
-                        onDelay = false;
-                        onDuration = true;
-                        duration = durations[sceneIndex];
-                        int size = sizes[sceneIndex];
-                        worldBorder.setSize(size, duration);
-                        return;
-                    }
-                    delay--;
-                } else if (onDuration) {
-                    Bukkit.getLogger().info(String.format("Shrinking to %d, %d/%d", sizes[sceneIndex], duration, durations[sceneIndex]));
-                    if (duration <= 0) {
-                        onDuration = false;
-                        onDelay = true;
-                        sceneIndex++;
-                        if (sceneIndex >= delays.length) {
-                            Bukkit.getLogger().info("Boarder is done shrinking");
-                            this.cancel();
-                            return;
-                        }
-                        delay = delays[sceneIndex];
-                        return;
-                    }
-                    duration--;
-                } else {
-                    //initialize
-                    onDelay = true;
-                    delay = delays[0];
-                }
-            }
-        }.runTaskTimer(plugin, 0L, 20L).getTaskId();
-    }
+    
+    
     
     
 }
