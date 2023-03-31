d48b55b50cefc80d81f438a604e3e6fceb779b40
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-03-31 15:35:17 -0400

    removed debug code

--- src/main/java/org/braekpo1nt/mctmanager/commands/MCTDebugCommand.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/MCTDebugCommand.java
@@ -34,51 +34,6 @@ public class MCTDebugCommand implements CommandExecutor {
             sender.sendMessage("This command can only be run by a player.");
             return true;
         }
-    
-        int startIndex = -1;
-        int endIndex = -1;
-    
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].startsWith("\"") && startIndex == -1) {
-                startIndex = i;
-                continue;
-            }
-        
-            if (args[i].endsWith("\"") && endIndex == -1) {
-                endIndex = i;
-            }
-        }
-    
-        if (startIndex == -1 || endIndex == -1 || startIndex > endIndex) {
-            sender.sendMessage("Usage: /mctdebug [args] <\"message text\"> [more args]");
-            return false;
-        }
-    
-        StringBuilder beforeBuilder = new StringBuilder();
-        for (int i = 0; i < startIndex; i++) {
-            beforeBuilder.append(args[i]);
-            beforeBuilder.append(" ");
-        }
-    
-        StringBuilder messageBuilder = new StringBuilder();
-        for (int i = startIndex; i <= endIndex; i++) {
-            messageBuilder.append(args[i]);
-            messageBuilder.append(" ");
-        }
-    
-        StringBuilder afterBuilder = new StringBuilder();
-        for (int i = endIndex + 1; i < args.length; i++) {
-            afterBuilder.append(args[i]);
-            afterBuilder.append(" ");
-        }
-        
-        sender.sendMessage("Before substring: " + beforeBuilder.toString().trim());
-        String messageWithQuotes = messageBuilder.toString().trim();
-        String message = messageWithQuotes.substring(1, messageWithQuotes.length() - 1).trim();
-        sender.sendMessage("Substring: " + message);
-        sender.sendMessage("After substring: " + afterBuilder.toString().trim());
-    
-        return true;
         
 //        Player player = ((Player) sender).getPlayer();
 //        Component mainTitle = Component.text("Main title");
@@ -87,7 +42,7 @@ public class MCTDebugCommand implements CommandExecutor {
 //        Title.Times times = Title.Times.times(Duration.ofMillis(500), Duration.ofSeconds(3), Duration.ofMillis(500));
 //        Title title = Title.title(mainTitle, subTitle, times);
 //        sender.showTitle(title);
-        
+        return true;
     }
     
     
