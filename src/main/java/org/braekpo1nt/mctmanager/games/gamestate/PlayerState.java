f784c6e7077e123ce8143a15c46ee98778ae89b9
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-04-02 23:20:48 -0400

    added documentation and moved PlayerState

--- src/main/java/org/braekpo1nt/mctmanager/games/gamestate/PlayerState.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/gamestate/PlayerState.java
@@ -1,4 +1,4 @@
-package org.braekpo1nt.mctmanager.games;
+package org.braekpo1nt.mctmanager.games.gamestate;
 
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -7,6 +7,13 @@ import org.bukkit.potion.PotionEffect;
 import java.util.Collection;
 import java.util.Collections;
 
+/**
+ * Used to store the state of a player mid-game when they leave or join.
+ * 
+ * @deprecated This might not be needed. Not sure if there are games where you need
+ * to store these things, or maybe it's just simpler to store it in a Player object. 
+ */
+@Deprecated(since = "4/2/2023", forRemoval = false)
 public class PlayerState {
     private final ItemStack[] inventoryContents;
     private final ItemStack[] armorContents;
@@ -17,6 +24,10 @@ public class PlayerState {
     private final int level;
     private final float exp;
     
+    /**
+     * The default state of a player. Essentially like respawning with a fresh
+     * start. 
+     */
     public static final PlayerState DEFAULT = new PlayerState(
             new ItemStack[]{},
             new ItemStack[]{},
@@ -28,6 +39,18 @@ public class PlayerState {
             0
     );
     
+    /**
+     * Instantiate a new PlayerState with the specified elements.
+     * 
+     * @param inventoryContents
+     * @param armorContents
+     * @param health
+     * @param foodLevel
+     * @param saturation
+     * @param activePotionEffects
+     * @param level
+     * @param exp
+     */
     private PlayerState(ItemStack[] inventoryContents, ItemStack[] armorContents, double health, int foodLevel, float saturation, Collection<PotionEffect> activePotionEffects, int level, float exp) {
         this.inventoryContents = inventoryContents;
         this.armorContents = armorContents;
@@ -39,6 +62,10 @@ public class PlayerState {
         this.exp = exp;
     }
     
+    /**
+     * Instantiates a new PlayerState with the state of the given player
+     * @param player The player whose state to save. 
+     */
     public PlayerState(Player player) {
         this.inventoryContents = player.getInventory().getContents().clone();
         this.armorContents = player.getEquipment().getArmorContents().clone();
@@ -50,6 +77,10 @@ public class PlayerState {
         this.exp = player.getExp();
     }
     
+    /**
+     * Replace the given player's state with this PlayerState
+     * @param player The player whose state you are replacing with this state
+     */
     public void setPlayerState(Player player) {
         player.getInventory().setContents(inventoryContents.clone());
         player.getEquipment().setArmorContents(armorContents.clone());
