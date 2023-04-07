5a051f928a776a3f84f93dbbe1a0166fe7b3ccfb
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-04-07 16:52:52 -0400

    added anchro

--- src/main/java/org/braekpo1nt/mctmanager/games/parkourpathway/ParkourPathwayGame.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/parkourpathway/ParkourPathwayGame.java
@@ -1,10 +1,14 @@
 package org.braekpo1nt.mctmanager.games.parkourpathway;
 
+import com.onarandombox.MultiverseCore.api.MVWorldManager;
+import com.onarandombox.MultiverseCore.utils.AnchorManager;
 import org.braekpo1nt.mctmanager.Main;
 import org.braekpo1nt.mctmanager.games.GameManager;
 import org.braekpo1nt.mctmanager.games.interfaces.MCTGame;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.potion.PotionEffect;
@@ -24,19 +28,25 @@ public class ParkourPathwayGame implements MCTGame, Listener {
     private final PotionEffect REGENERATION = new PotionEffect(PotionEffectType.REGENERATION, 70, 200, true, false, false);
     private final PotionEffect FIRE_RESISTANCE = new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 70, 1, true, false, false);
     private final PotionEffect SATURATION = new PotionEffect(PotionEffectType.SATURATION, 70, 250, true, false, false);
-    private boolean gameActive = false;
+    private final World parkourPathwayWorld;
     private int statusEffectsTaskId;
+    private boolean gameActive = false;
     private List<Player> participants;
+    private Location parkourPathwayStartAnchor;
 
     public ParkourPathwayGame(Main plugin, GameManager gameManager) {
         this.plugin = plugin;
         this.gameManager = gameManager;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        MVWorldManager worldManager = Main.multiverseCore.getMVWorldManager();
+        this.parkourPathwayWorld = worldManager.getMVWorld("FT").getCBWorld();
     }
     
     @Override
     public void start(List<Player> newParticipants) {
         participants = new ArrayList<>();
+        AnchorManager anchorManager = Main.multiverseCore.getAnchorManager();
+        this.parkourPathwayStartAnchor = anchorManager.getAnchorLocation("parkour-pathway");
         for (Player participant : newParticipants) {
             initializeParticipant(participant);
         }
