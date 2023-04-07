2a0fb744ba425a48a9968cd68c27977ad6179469
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-04-07 10:45:08 -0400

    finished start

--- src/main/java/org/braekpo1nt/mctmanager/games/spleef/SpleefGame.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/spleef/SpleefGame.java
@@ -2,17 +2,22 @@ package org.braekpo1nt.mctmanager.games.spleef;
 
 import com.onarandombox.MultiverseCore.api.MVWorldManager;
 import com.onarandombox.MultiverseCore.utils.AnchorManager;
+import net.kyori.adventure.text.Component;
 import org.braekpo1nt.mctmanager.Main;
 import org.braekpo1nt.mctmanager.games.GameManager;
 import org.braekpo1nt.mctmanager.games.interfaces.MCTGame;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.NamespacedKey;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.structure.Mirror;
 import org.bukkit.block.structure.StructureRotation;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 import org.bukkit.structure.Structure;
 
 import java.util.*;
@@ -24,7 +29,11 @@ public class SpleefGame implements MCTGame, Listener {
     private final World spleefWorld;
     private Map<UUID, Boolean> participantsAlive;
     private boolean gameActive = false;
+    private boolean spleefStarted = false;
     private Location spleefStartAnchor;
+    private final PotionEffect SATURATION = new PotionEffect(PotionEffectType.SATURATION, 70, 250, true, false, false);
+    private int statusEffectsTaskId;
+    private int startCountDownTaskID;
 
     public SpleefGame(Main plugin, GameManager gameManager) {
         this.plugin = plugin;
@@ -38,24 +47,30 @@ public class SpleefGame implements MCTGame, Listener {
     public void start(List<Player> newParticipants) {
         this.participants = new ArrayList<>();
         participantsAlive = new HashMap<>();
+        AnchorManager anchorManager = Main.multiverseCore.getAnchorManager();
+        this.spleefStartAnchor = anchorManager.getAnchorLocation("spleef");
         placeLayers();
         for (Player participant : newParticipants) {
             initializeParticipant(participant);
         }
+        startStatusEffectsTask();
+        startStartSpleefCountDownTask();
+        setupTeamOptions();
         gameActive = true;
+        Bukkit.getLogger().info("Starting Spleef game");
     }
-
+    
     private void initializeParticipant(Player participant) {
         UUID participantUniqueId = participant.getUniqueId();
         participants.add(participant);
         participantsAlive.put(participantUniqueId, true);
-        AnchorManager anchorManager = Main.multiverseCore.getAnchorManager();
-        this.spleefStartAnchor = anchorManager.getAnchorLocation("spleef");
+        
     }
 
     @Override
     public void stop() {
-
+        gameActive = false;
+        spleefStarted = false;
     }
 
     @Override
@@ -68,6 +83,48 @@ public class SpleefGame implements MCTGame, Listener {
 
     }
     
+    private void startSpleef() {
+        placeLayers();
+        givePlayersShovels();
+        spleefStarted = true;
+    }
+
+    private void givePlayersShovels() {
+        for (Player participant : participants) {
+            giveParticipantShovel(participant);
+        }
+    }
+
+    private void giveParticipantShovel(Player participant) {
+        ItemStack diamondShovel = new ItemStack(Material.DIAMOND_SHOVEL);
+        diamondShovel.addEnchantment(Enchantment.DIG_SPEED, 5);
+        diamondShovel.addUnsafeEnchantment(Enchantment.DURABILITY, 10);
+        participant.getInventory().addItem(diamondShovel);
+    }
+
+    private void startStartSpleefCountDownTask() {
+        this.startCountDownTaskID = new BukkitRunnable() {
+            private int count = 10;
+
+            @Override
+            public void run() {
+                for (Player participant : participants) {
+                    if (count <= 0) {
+                        participant.sendMessage(Component.text("Go!"));
+                    } else {
+                        participant.sendMessage(Component.text(count));
+                    }
+                }
+                if (count <= 0) {
+                    startSpleef();
+                    this.cancel();
+                    return;
+                }
+                count--;
+            }
+        }.runTaskTimer(plugin, 0L, 20L).getTaskId();
+    }
+    
     private void placeLayers() {
         Structure layer1 = Bukkit.getStructureManager().loadStructure(new NamespacedKey("mctdatapack", "spleef/spleef_layer1"));
         Structure layer2 = Bukkit.getStructureManager().loadStructure(new NamespacedKey("mctdatapack", "spleef/spleef_layer2"));
@@ -78,5 +135,27 @@ public class SpleefGame implements MCTGame, Listener {
         layer2.place(new Location(spleefWorld, -15, 16, -2015), true, StructureRotation.NONE, Mirror.NONE, 0, 1, new Random());
         layer3.place(new Location(spleefWorld, -15, 11, -2015), true, StructureRotation.NONE, Mirror.NONE, 0, 1, new Random());
         layer4.place(new Location(spleefWorld, -15, 6, -2015), true, StructureRotation.NONE, Mirror.NONE, 0, 1, new Random());
+    }
+
+    private void startStatusEffectsTask() {
+        this.statusEffectsTaskId = new BukkitRunnable(){
+            @Override
+            public void run() {
+                for (Player participant : participants) {
+                    participant.addPotionEffect(SATURATION);
+                }
+            }
+        }.runTaskTimer(plugin, 0L, 60L).getTaskId();
+    }
+
+    private void setupTeamOptions() {
+        Scoreboard mctScoreboard = gameManager.getMctScoreboard();
+        for (Team team : mctScoreboard.getTeams()) {
+            team.setAllowFriendlyFire(false);
+            team.setCanSeeFriendlyInvisibles(true);
+            team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.ALWAYS);
+            team.setOption(Team.Option.DEATH_MESSAGE_VISIBILITY, Team.OptionStatus.ALWAYS);
+            team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
+        }
     }
 }
