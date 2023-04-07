c1154d1d78e045bf920480941101f523c19fa08c
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-04-07 16:47:45 -0400

    added method placeholders

--- src/main/java/org/braekpo1nt/mctmanager/games/parkourpathway/ParkourPathwayGame.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/parkourpathway/ParkourPathwayGame.java
@@ -3,16 +3,30 @@ package org.braekpo1nt.mctmanager.games.parkourpathway;
 import org.braekpo1nt.mctmanager.Main;
 import org.braekpo1nt.mctmanager.games.GameManager;
 import org.braekpo1nt.mctmanager.games.interfaces.MCTGame;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class ParkourPathwayGame implements MCTGame, Listener {
 
     private final Main plugin;
     private final GameManager gameManager;
+    private final PotionEffect INVISIBILITY = new PotionEffect(PotionEffectType.INVISIBILITY, 10000, 1, true, false, false);
+    private final PotionEffect RESISTANCE = new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 70, 200, true, false, false);
+    private final PotionEffect REGENERATION = new PotionEffect(PotionEffectType.REGENERATION, 70, 200, true, false, false);
+    private final PotionEffect FIRE_RESISTANCE = new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 70, 1, true, false, false);
+    private final PotionEffect SATURATION = new PotionEffect(PotionEffectType.SATURATION, 70, 250, true, false, false);
     private boolean gameActive = false;
+    private int statusEffectsTaskId;
+    private List<Player> participants;
 
     public ParkourPathwayGame(Main plugin, GameManager gameManager) {
         this.plugin = plugin;
@@ -22,12 +36,37 @@ public class ParkourPathwayGame implements MCTGame, Listener {
     
     @Override
     public void start(List<Player> newParticipants) {
+        participants = new ArrayList<>();
+        for (Player participant : newParticipants) {
+            initializeParticipant(participant);
+        }
+        startStatusEffectsTask();
+        setupTeamOptions();
+        startParkourPathwayTimer();
         gameActive = true;
+        Bukkit.getLogger().info("Starting Parkour Pathway game");
+    }
+
+    private void initializeParticipant(Player participant) {
+        UUID participantUniqueId = participant.getUniqueId();
+        participants.add(participant);
+        initializeFastBoard(participant);
+        teleportPlayerToStartingPosition(participant);
+        participant.getInventory().clear();
+        participant.setGameMode(GameMode.ADVENTURE);
+        clearStatusEffects(participant);
+        resetHealthAndHunger(participant);
     }
 
     @Override
     public void stop() {
+        cancelAlltasks();
+        for (Player participant : participants) {
+            resetParticipant(participant);
+        }
+        participants.clear();
         gameActive = false;
+        Bukkit.getLogger().info("Stopping Parkour Pathway game");
     }
 
     @Override
@@ -38,5 +77,20 @@ public class ParkourPathwayGame implements MCTGame, Listener {
     @Override
     public void onParticipantQuit(Player participant) {
 
+    }
+
+    private void startStatusEffectsTask() {
+        this.statusEffectsTaskId = new BukkitRunnable(){
+            @Override
+            public void run() {
+                for (Player participant : participants) {
+                    participant.addPotionEffect(INVISIBILITY);
+                    participant.addPotionEffect(RESISTANCE);
+                    participant.addPotionEffect(REGENERATION);
+                    participant.addPotionEffect(FIRE_RESISTANCE);
+                    participant.addPotionEffect(SATURATION);
+                }
+            }
+        }.runTaskTimer(plugin, 0L, 60L).getTaskId();
     }
 }
