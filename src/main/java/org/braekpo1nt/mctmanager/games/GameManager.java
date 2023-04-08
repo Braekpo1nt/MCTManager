d0e8dec408fa96e51a75e49a1c04526ff6f41372
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-04-08 01:03:28 -0400

    added voting pool command

--- src/main/java/org/braekpo1nt/mctmanager/games/GameManager.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/GameManager.java
@@ -164,7 +164,7 @@ public class GameManager implements Listener {
         gameStateStorageUtil.saveGameState();
     }
     
-    public void startVote(@NotNull CommandSender sender) {
+    public void startVote(@NotNull CommandSender sender, List<MCTGames> votingPool) {
         List<Player> onlineParticipants = this.getOnlineParticipants();
         if (onlineParticipants.isEmpty()) {
             sender.sendMessage(Component.text("There are no online participants. You can add participants using:\n")
