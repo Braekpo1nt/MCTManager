316f254d00ad506bb9baf30809e35385aae68789
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-04-18 14:55:43 -0400

    successfully got a basic MockBukkit test to pass

--- src/test/java/org/braekpo1nt/mctmanager/MainTest.java
+++ src/test/java/org/braekpo1nt/mctmanager/MainTest.java
@@ -2,8 +2,7 @@ package org.braekpo1nt.mctmanager;
 
 import be.seeseemelk.mockbukkit.MockBukkit;
 import be.seeseemelk.mockbukkit.ServerMock;
-import com.onarandombox.MultiverseCore.MultiverseCore;
-import org.bukkit.plugin.Plugin;
+import be.seeseemelk.mockbukkit.UnimplementedOperationException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,13 +15,14 @@ public class MainTest {
     
     @BeforeEach
     public void setUp() {
-        // Start the mock server
-        server = MockBukkit.mock();
-        server.addSimpleWorld("world");
-        Plugin mv = MockBukkit.load(MultiverseCore.class);
-        
-        // Load your plugin
-        plugin = MockBukkit.load(Main.class);
+        server = MockBukkit.mock(new MyCustomServerMock());
+        try {
+            plugin = MockBukkit.load(Main.class);
+        } catch (UnimplementedOperationException ex) {
+            System.out.println("UnimplementedOperationException while setting up MainTest. MockBukkit must not support the functionality/operation you are trying to test. Check the stack trace below for the exact method that threw the exception. Message from exception:" + ex.getMessage());
+            ex.printStackTrace();
+            System.exit(1);
+        }
     }
     
     @AfterEach
@@ -35,7 +35,7 @@ public class MainTest {
         int three = 3;
         int four = 4;
         int sum = three + four;
-        Assertions.assertEquals(6, sum);
+        Assertions.assertEquals(7, sum);
     }
     
 }
