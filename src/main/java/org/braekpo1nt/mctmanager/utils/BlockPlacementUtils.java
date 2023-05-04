2387450d17be96dfdb569c62fe0e9be84b75082a
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-05-04 17:21:43 -0400

    fixed directional glass

--- src/main/java/org/braekpo1nt/mctmanager/utils/BlockPlacementUtils.java
+++ src/main/java/org/braekpo1nt/mctmanager/utils/BlockPlacementUtils.java
@@ -5,7 +5,9 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Directional;
+import org.bukkit.block.data.MultipleFacing;
 
 public class BlockPlacementUtils {
     public static void createCube(World world, int xOrigin, int yOrigin, int zOrigin, int xSize, int ySize, int zSize, Material blockType) {
@@ -57,30 +59,24 @@ public class BlockPlacementUtils {
         updateDirection(world, xOrigin, yOrigin, zOrigin, xSize, ySize, zSize);
     }
     
+    private static final BlockFace[] cardinalDirections = new BlockFace[]{BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST};
+    
     public static void updateDirection(World world, int xOrigin, int yOrigin, int zOrigin, int xSize, int ySize, int zSize) {
-        for (int x = xOrigin; x < xOrigin + xSize; x++) {
-            for (int y = yOrigin; y < yOrigin + ySize; y++) {
-                for (int z = zOrigin; z < zOrigin + zSize; z++) {
+        int xEnd = xOrigin + xSize - 1;
+        int yEnd = yOrigin + ySize - 1;
+        int zEnd = zOrigin + zSize - 1;
+        
+        for (int x = xOrigin; x <= xEnd; x++) {
+            for (int y = yOrigin; y <= yEnd; y++) {
+                for (int z = zOrigin; z <= zEnd; z++) {
                     Block block = world.getBlockAt(x, y, z);
-                    if (block.getBlockData() instanceof Directional directional) {
-                        boolean north = false;
-                        boolean east = false;
-                        boolean south = false;
-                        boolean west = false;
-                        if (block.getRelative(BlockFace.NORTH).getType() == directional.getMaterial() && block.getRelative(BlockFace.NORTH).getBlockData() instanceof Directional northBlockData) {
-                            north = northBlockData.getFacing() == BlockFace.SOUTH;
+                    if (block.getBlockData() instanceof MultipleFacing multipleFacing) {
+                        for (BlockFace direction : cardinalDirections) {
+                            if (block.getRelative(direction).isSolid()) {
+                                multipleFacing.setFace(direction, true);
+                                block.setBlockData(multipleFacing);
+                            }
                         }
-                        if (block.getRelative(BlockFace.EAST).getType() == directional.getMaterial() && block.getRelative(BlockFace.EAST).getBlockData() instanceof Directional eastBlockData) {
-                            east = eastBlockData.getFacing() == BlockFace.WEST;
-                        }
-                        if (block.getRelative(BlockFace.SOUTH).getType() == directional.getMaterial() && block.getRelative(BlockFace.SOUTH).getBlockData() instanceof Directional southBlockData) {
-                            south = southBlockData.getFacing() == BlockFace.NORTH;
-                        }
-                        if (block.getRelative(BlockFace.WEST).getType() == directional.getMaterial() && block.getRelative(BlockFace.WEST).getBlockData() instanceof Directional westBlockData) {
-                            west = westBlockData.getFacing() == BlockFace.EAST;
-                        }
-                        directional.setFacing(north ? BlockFace.NORTH : south ? BlockFace.SOUTH : east ? BlockFace.EAST : west ? BlockFace.WEST : directional.getFacing());
-                        block.setBlockData(directional);
                     }
                 }
             }
