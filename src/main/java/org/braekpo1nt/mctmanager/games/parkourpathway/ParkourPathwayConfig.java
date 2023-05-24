24ca197c893233f1245a349de11d9d343091ffda
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-05-24 18:47:51 -0400

    added config storage util for parkour

---
+++ src/main/java/org/braekpo1nt/mctmanager/games/parkourpathway/ParkourPathwayConfig.java
@@ -0,0 +1,12 @@
+package org.braekpo1nt.mctmanager.games.parkourpathway;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParkourPathwayConfig {
+    protected List<List<CheckPoint>> checkpoints;
+    
+    public ParkourPathwayConfig() {
+        this.checkpoints = new ArrayList<>();
+    }
+}
