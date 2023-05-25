e63fd9c912d7780e6dd180bfa1825c94e14a7730
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-05-25 11:50:58 -0400

    added start outline

--- src/main/java/org/braekpo1nt/mctmanager/games/clockwork/ClockWorkGame.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/clockwork/ClockWorkGame.java
@@ -4,42 +4,62 @@ import org.braekpo1nt.mctmanager.Main;
 import org.braekpo1nt.mctmanager.games.GameManager;
 import org.braekpo1nt.mctmanager.games.enums.MCTGames;
 import org.braekpo1nt.mctmanager.games.interfaces.MCTGame;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 
-import java.util.List;
+import java.util.*;
 
-public class ClockWorkGame implements MCTGame {
+public class ClockWorkGame implements MCTGame, Listener {
     private final Main plugin;
     private final GameManager gameManager;
+    private List<Player> participants;
+    private Map<UUID, Boolean> participantsAreAlive;
 
     public ClockWorkGame(Main plugin, GameManager gameManager) {
-
         this.plugin = plugin;
         this.gameManager = gameManager;
     }
-
+    
     @Override
     public MCTGames getType() {
         return MCTGames.CLOCKWORK;
     }
-
+    
     @Override
     public void start(List<Player> newParticipants) {
+        this.participants = new ArrayList<>(newParticipants.size());
+        participantsAreAlive = new HashMap<>(newParticipants.size());
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        for (Player participant : newParticipants) {
+            initializeParticipant(participant);
+        }
+        setUpTeamOptions();
+        startCountDownTask();
+        gameActive = true;
+        Bukkit.getLogger().info("Started Clockwork");
+    }
 
+    private void initializeParticipant(Player participant) {
+        participants.add(participant);
+        UUID participantUniqueId = participant.getUniqueId();
+        participantsAreAlive.put(participantUniqueId, true);
+        participant.teleport(this.startingPosition);
+        
     }
 
     @Override
     public void stop() {
-
+        
     }
-
+    
     @Override
     public void onParticipantJoin(Player participant) {
-
+        
     }
-
+    
     @Override
     public void onParticipantQuit(Player participant) {
-
+        
     }
 }
