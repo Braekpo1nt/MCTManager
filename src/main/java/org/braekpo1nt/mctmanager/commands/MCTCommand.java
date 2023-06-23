1660c37d63ab8655b29a0edf797a9dd95f3c1c7d
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-06-23 17:22:32 -0400

    added barbones for admin sub command

--- src/main/java/org/braekpo1nt/mctmanager/commands/MCTCommand.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/MCTCommand.java
@@ -2,6 +2,7 @@ package org.braekpo1nt.mctmanager.commands;
 
 import net.kyori.adventure.text.Component;
 import org.braekpo1nt.mctmanager.Main;
+import org.braekpo1nt.mctmanager.commands.admin.AdminSubCommand;
 import org.braekpo1nt.mctmanager.commands.event.EventSubCommand;
 import org.braekpo1nt.mctmanager.commands.game.GameSubCommand;
 import org.braekpo1nt.mctmanager.commands.team.TeamSubCommand;
@@ -16,12 +17,13 @@ import java.io.IOException;
  * Everything should start with /mct _____, where _____ is a sub command
  */
 public class MCTCommand extends CommandManager {
-
+    
     public MCTCommand(Main plugin, GameManager gameManager, BlockEffectsListener blockEffectsListener) {
         plugin.getCommand("mct").setExecutor(this);
         subCommands.put("game", new GameSubCommand(gameManager));
         subCommands.put("option", new OptionSubCommand(gameManager, blockEffectsListener));
         subCommands.put("team", new TeamSubCommand(gameManager));
+        subCommands.put("admin", new AdminSubCommand(gameManager));
         subCommands.put("event", new EventSubCommand(gameManager));
         subCommands.put("save", (sender, command, label, args) -> {
             gameManager.saveGameState();
@@ -34,7 +36,6 @@ public class MCTCommand extends CommandManager {
             return true;
         });
     }
-    
     
     @Override
     public Component getUsageMessage() {
---
+++ src/main/java/org/braekpo1nt/mctmanager/commands/admin/AddSubCommand.java
@@ -0,0 +1,28 @@
+package org.braekpo1nt.mctmanager.commands.admin;
+
+import org.braekpo1nt.mctmanager.games.GameManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class AddSubCommand implements TabExecutor {
+    private final GameManager gameManager;
+    
+    public AddSubCommand(GameManager gameManager) {
+        this.gameManager = gameManager;
+    }
+    
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+    
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return null;
+    }
+}
