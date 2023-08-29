2f7667e7f5c2a25144851a123cb8319cc56aeaf3
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-08-29 15:46:03 -0400

    added javadoc to borderstage

--- src/main/java/org/braekpo1nt/mctmanager/games/game/mecha/config/BorderStage.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/mecha/config/BorderStage.java
@@ -1,3 +1,9 @@
 package org.braekpo1nt.mctmanager.games.game.mecha.config;
 
+/**
+ * 
+ * @param size The size (in blocks) the border will be at this stage. The border will shrink from the previous stage's size to this stage's size over the previous stage's duration
+ * @param delay the border will stay at this stage's size for this many seconds
+ * @param duration after the delay, the border will shrink for this many seconds to the next stage's size
+ */
 public record BorderStage (int size, int delay, int duration){}
