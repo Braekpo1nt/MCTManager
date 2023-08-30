fae08f24c5331f01ad6556be7f0d72e1497bac41
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-08-30 13:31:27 -0400

    added capture the flag test

---
+++ src/test/java/org/braekpo1nt/mctmanager/games/game/capturetheflag/config/CaptureTheFlagStorageUtilTest.java
@@ -0,0 +1,61 @@
+package org.braekpo1nt.mctmanager.games.game.capturetheflag.config;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import org.braekpo1nt.mctmanager.Main;
+import org.braekpo1nt.mctmanager.MyCustomServerMock;
+import org.braekpo1nt.mctmanager.TestUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.logging.Level;
+
+public class CaptureTheFlagStorageUtilTest {
+    String validConfigFile = "validCaptureTheFlagConfig.json";
+    String invalidConfigFile = "invalidCaptureTheFlagConfig.json";
+    String configFileName = "captureTheFlagConfig.json";
+    Main plugin;
+    CaptureTheFlagStorageUtil storageUtil;
+    
+    @BeforeEach
+    void setupServerAndPlugin() {
+        ServerMock server = MockBukkit.mock(new MyCustomServerMock());
+        server.getLogger().setLevel(Level.OFF);
+        plugin = MockBukkit.load(Main.class);
+        storageUtil = new CaptureTheFlagStorageUtil(plugin.getDataFolder());
+    }
+    
+    @AfterEach
+    void tearDown() {
+        MockBukkit.unmock();
+    }
+    
+    @Test
+    void configDoesNotExist() {
+        Assertions.assertThrows(IllegalArgumentException.class, storageUtil::loadConfig);
+    }
+    
+    @Test
+    void malformedJson() {
+        TestUtils.createFileInDirectory(plugin.getDataFolder(), configFileName, "{,");
+        Assertions.assertThrows(IllegalArgumentException.class, storageUtil::loadConfig);
+    }
+    
+    @Test
+    void wellFormedJsonValidData() {
+        InputStream inputStream = getClass().getResourceAsStream(validConfigFile);
+        TestUtils.copyInputStreamToFile(inputStream, new File(plugin.getDataFolder(), configFileName));
+        Assertions.assertTrue(storageUtil.loadConfig());
+    }
+    
+    @Test
+    void wellFormedJsonInvalidData() {
+        InputStream inputStream = getClass().getResourceAsStream(invalidConfigFile);
+        TestUtils.copyInputStreamToFile(inputStream, new File(plugin.getDataFolder(), configFileName));
+        Assertions.assertThrows(IllegalArgumentException.class, storageUtil::loadConfig);
+    }
+}
