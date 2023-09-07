8cdb18c0c4d8c8deb217897633837f32e12a8ce2
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-09-06 23:04:35 -0400

    modified capturethe flag

--- src/test/java/org/braekpo1nt/mctmanager/games/game/capturetheflag/config/CaptureTheFlagStorageUtilTest.java
+++ src/test/java/org/braekpo1nt/mctmanager/games/game/capturetheflag/config/CaptureTheFlagStorageUtilTest.java
@@ -2,6 +2,7 @@ package org.braekpo1nt.mctmanager.games.game.capturetheflag.config;
 
 import be.seeseemelk.mockbukkit.MockBukkit;
 import be.seeseemelk.mockbukkit.ServerMock;
+import com.google.gson.JsonObject;
 import org.braekpo1nt.mctmanager.Main;
 import org.braekpo1nt.mctmanager.MyCustomServerMock;
 import org.braekpo1nt.mctmanager.TestUtils;
@@ -47,22 +48,24 @@ class CaptureTheFlagStorageUtilTest {
     
     @Test
     void wellFormedJsonValidData() {
-        InputStream inputStream = getClass().getResourceAsStream(validConfigFile);
+        InputStream inputStream = storageUtil.getExampleResourceStream();
         TestUtils.copyInputStreamToFile(inputStream, new File(plugin.getDataFolder(), configFileName));
         Assertions.assertTrue(storageUtil.loadConfig());
     }
     
     @Test
     void wellFormedJsonInvalidData() {
-        InputStream inputStream = getClass().getResourceAsStream(invalidConfigFile);
-        TestUtils.copyInputStreamToFile(inputStream, new File(plugin.getDataFolder(), configFileName));
-        Assertions.assertThrows(IllegalArgumentException.class, storageUtil::loadConfig);
-    }
-    
-    @Test
-    void exampleJson() {
         InputStream inputStream = storageUtil.getExampleResourceStream();
-        TestUtils.copyInputStreamToFile(inputStream, new File(plugin.getDataFolder(), configFileName));
-        Assertions.assertTrue(storageUtil.loadConfig());
+        JsonObject json = TestUtils.inputStreamToJson(inputStream);
+        JsonObject spectatorArea = new JsonObject();
+        spectatorArea.addProperty("minX", 0);
+        spectatorArea.addProperty("minY", 0);
+        spectatorArea.addProperty("minZ", 0);
+        spectatorArea.addProperty("maxX", 0);
+        spectatorArea.addProperty("maxY", 0);
+        spectatorArea.addProperty("maxZ", 0);
+        json.add("spectatorArea", spectatorArea);
+        TestUtils.saveJsonToFile(json, new File(plugin.getDataFolder(), configFileName));
+        Assertions.assertThrows(IllegalArgumentException.class, storageUtil::loadConfig);
     }
 }
