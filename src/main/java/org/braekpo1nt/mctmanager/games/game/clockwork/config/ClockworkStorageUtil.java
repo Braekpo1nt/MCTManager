9c7c10edfcb02f1ab9f0890f44e6b3cf57b3bd9d
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-09-08 15:36:17 -0400

    made setconfig and getters

--- src/main/java/org/braekpo1nt/mctmanager/games/game/clockwork/config/ClockworkStorageUtil.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/clockwork/config/ClockworkStorageUtil.java
@@ -5,17 +5,25 @@ import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import org.braekpo1nt.mctmanager.Main;
+import org.braekpo1nt.mctmanager.games.game.clockwork.Wedge;
 import org.braekpo1nt.mctmanager.games.game.config.BoundingBoxDTO;
 import org.braekpo1nt.mctmanager.games.game.config.GameConfigStorageUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClockworkStorageUtil extends GameConfigStorageUtil<ClockworkConfig> {
     
     private ClockworkConfig clockworkConfig;
+    private World world;
+    private Location startingLocation;
+    private List<Wedge> wedges;
     
     /**
      * @param configDirectory The directory that the config should be located in (e.g. the plugin's data folder)
@@ -58,11 +66,50 @@ public class ClockworkStorageUtil extends GameConfigStorageUtil<ClockworkConfig>
     
     @Override
     protected void setConfig(ClockworkConfig config) throws IllegalArgumentException {
+        World newWorld = Bukkit.getWorld(config.world());
+        Preconditions.checkArgument(newWorld != null, "Could not find world \"%s\"", config.world());
+        Location newStartingLocation = config.startingLocation().toLocation(newWorld);
+        List<Wedge> newWedges = new ArrayList<>(config.wedges().size());
+        for (ClockworkConfig.WedgeDTO wedgeDTO : config.wedges()) {
+            newWedges.add(new Wedge(wedgeDTO.getDetectionArea()));
+        }
+        // now it's confirmed everything works, so set the actual fields
+        this.world = newWorld;
+        this.startingLocation = newStartingLocation;
+        this.wedges = newWedges;
         this.clockworkConfig = config;
     }
     
     @Override
     protected InputStream getExampleResourceStream() {
         return ClockworkStorageUtil.class.getResourceAsStream("exampleClockworkConfig.json");
+    }
+    
+    public World getWorld() {
+        return world;
+    }
+    
+    public Location getStartingLocation() {
+        return startingLocation;
+    }
+    
+    public List<Wedge> getWedges() {
+        return wedges;
+    }
+    
+    public int getRounds() {
+        return clockworkConfig.rounds();
+    }
+    
+    public int getPlayerEliminationScore() {
+        return clockworkConfig.scores().playerElimination();
+    }
+    
+    public int getTeamEliminationScore() {
+        return clockworkConfig.scores().teamElimination();
+    }
+    
+    public int getWinRoundScore() {
+        return clockworkConfig.scores().winRound();
     }
 }
