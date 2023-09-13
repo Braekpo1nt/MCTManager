dcc19180b570f546e2c5a9224f8d479384ab96cf
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-09-13 16:14:21 -0400

    summons arrows over time

--- src/main/java/org/braekpo1nt/mctmanager/games/game/clockwork/ChaosManager.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/clockwork/ChaosManager.java
@@ -11,20 +11,21 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import java.util.Random;
 
 public class ChaosManager implements Listener {
     private int minArrows = 1;
-    private int maxArrows = 1;
-    private long minDelay = 40L;
-    private long maxDelay = 80L;
+    private int maxArrows = 5;
+    private long minDelay = 20L;
+    private long maxDelay = 40L;
     private Location center;
     private int radius = 22;
     private int minY = 7;
     private int maxY = 13;
-    private int arrowsTaskId;
+    private int scheduleArrowsSummonTaskId;
     private final Random random = new Random();
     
     private final Main plugin;
@@ -36,13 +37,43 @@ public class ChaosManager implements Listener {
     
     public void start() {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
-        this.arrowsTaskId = Bukkit.getScheduler().runTaskTimer(plugin, this::summonArrows, 0L, random.nextLong() % (maxDelay - minDelay + 1) + minDelay).getTaskId();
+//        this.arrowsTaskId = Bukkit.getScheduler().runTaskTimer(plugin, this::summonArrows, 0L, random.nextLong() % (maxDelay - minDelay + 1) + minDelay).getTaskId();
+        scheduleArrowSummonTask();
     }
     
     public void stop() {
         HandlerList.unregisterAll(this);
-        Bukkit.getScheduler().cancelTask(arrowsTaskId);
+        cancelAllTasks();
         removeAllArrowsInCylinder();
+    }
+    
+    private void scheduleArrowSummonTask() {
+        long randomDelay = random.nextLong(minDelay, maxDelay);
+        this.scheduleArrowsSummonTaskId = new BukkitRunnable() {
+            @Override
+            public void run() {
+                summonArrows();
+                scheduleArrowSummonTask();
+            }
+        }.runTaskLater(plugin, randomDelay).getTaskId();
+    }
+    
+    private void summonArrows() {
+        int numArrows = random.nextInt(minArrows, maxArrows + 1);
+        World world = center.getWorld();
+        for (int i = 0; i < numArrows; i++) {
+            double randomRadius = radius * Math.sqrt(random.nextDouble()); // generate a "uniformly random" radius (see https://stackoverflow.com/questions/5837572/generate-a-random-point-within-a-circle-uniformly)
+            double randomAngle = random.nextDouble() * 2 * Math.PI;
+            double x = center.getX() + randomRadius * Math.cos(randomAngle);
+            double z = center.getZ() + randomRadius * Math.sin(randomAngle);
+            double y = random.nextDouble(minY, maxY);
+            Location spawnLocation = new Location(world, x, y, z);
+            if (spawnLocation.getBlock().getType().equals(Material.AIR)) {
+                Arrow arrow = world.spawnArrow(spawnLocation, new Vector(0, -1, 0), 0.6f, 12);
+                arrow.setGravity(true);
+                arrow.setPickupStatus(Arrow.PickupStatus.DISALLOWED);
+            }
+        }
     }
     
     @EventHandler
@@ -53,22 +84,8 @@ public class ChaosManager implements Listener {
         }
     }
     
-    private void summonArrows() {
-        int numArrows = new Random().nextInt(maxArrows - minArrows + 1) + minArrows;
-        World world = center.getWorld();
-        for (int i = 0; i < numArrows; i++) {
-            double randomRadius = radius * Math.sqrt(random.nextDouble()); // generate a "uniformly random" radius (see https://stackoverflow.com/questions/5837572/generate-a-random-point-within-a-circle-uniformly)
-            double randomAngle = random.nextDouble() * 2 * Math.PI;
-            double x = center.getX() + randomRadius * Math.cos(randomAngle);
-            double z = center.getZ() + randomRadius * Math.sin(randomAngle);
-            double y = Math.random() * (maxY - minY + 1) + minY; // Random y within the defined range
-            Location spawnLocation = new Location(world, x, y, z);
-            if (spawnLocation.getBlock().getType().equals(Material.AIR)) {
-                Arrow arrow = world.spawnArrow(spawnLocation, new Vector(0, -1, 0), 0, 0);
-                arrow.setGravity(true);
-                arrow.setPickupStatus(Arrow.PickupStatus.DISALLOWED);
-            }
-        }
+    private void cancelAllTasks() {
+        Bukkit.getScheduler().cancelTask(scheduleArrowsSummonTaskId);
     }
     
     private void removeAllArrowsInCylinder() {
@@ -76,20 +93,5 @@ public class ChaosManager implements Listener {
         for (Arrow arrow : world.getNearbyEntitiesByType(Arrow.class, center, radius)) {
             arrow.remove();
         }
-    }
-    
-    private void scheduleArrowSummon() {
-        this.arrowsTaskId = Bukkit.getScheduler().runTaskTimer(plugin, () -> {
-            int numArrows = random.nextInt(maxArrows - minArrows + 1) + minArrows;
-            World world = center.getWorld();
-            for (int i = 0; i < numArrows; i++) {
-                double angle = Math.random() * Math.PI * 2;
-                double x = center.getX() + Math.cos(angle) * radius;
-                double z = center.getZ() + Math.sin(angle) * radius;
-                double y = Math.random() * (maxY - minY + 1) + minY;
-                Location spawnLocation = new Location(world, x, y, z);
-                world.spawnEntity(spawnLocation, EntityType.ARROW);
-            }
-        }, minDelay, random.nextLong() % (maxDelay - minDelay + 1) + minDelay).getTaskId();
     }
 }
