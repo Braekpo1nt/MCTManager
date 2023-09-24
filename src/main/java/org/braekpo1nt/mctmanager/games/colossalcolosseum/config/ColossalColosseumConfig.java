d65dfe9c373f00b7cab61f98ca22917f10ad4856
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-09-24 12:26:56 -0400

    added validator for existing attributes

--- src/main/java/org/braekpo1nt/mctmanager/games/colossalcolosseum/config/ColossalColosseumConfig.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/colossalcolosseum/config/ColossalColosseumConfig.java
@@ -1,6 +1,7 @@
 package org.braekpo1nt.mctmanager.games.colossalcolosseum.config;
 
+import com.google.gson.JsonObject;
 import org.braekpo1nt.mctmanager.games.game.config.LocationDTO;
 
-record ColossalColosseumConfig(String version, String world, LocationDTO firstPlaceSpawn, LocationDTO secondPlaceSpawn, LocationDTO spectatorSpawn) {
+record ColossalColosseumConfig(String version, String world, LocationDTO firstPlaceSpawn, LocationDTO secondPlaceSpawn, LocationDTO spectatorSpawn, JsonObject description) {
 }
--- src/main/java/org/braekpo1nt/mctmanager/games/colossalcolosseum/config/ColossalColosseumStorageUtil.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/colossalcolosseum/config/ColossalColosseumStorageUtil.java
@@ -1,5 +1,10 @@
 package org.braekpo1nt.mctmanager.games.colossalcolosseum.config;
 
+import com.google.common.base.Preconditions;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import org.braekpo1nt.mctmanager.Main;
 import org.braekpo1nt.mctmanager.games.game.config.GameConfigStorageUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -23,15 +28,27 @@ public class ColossalColosseumStorageUtil extends GameConfigStorageUtil<Colossal
     public ColossalColosseumStorageUtil(File configDirectory) {
         super(configDirectory, "colossalColosseum.json", ColossalColosseumConfig.class);
     }
-
+    
     @Override
     protected ColossalColosseumConfig getConfig() {
-        return null;
+        return colossalColosseumConfig;
     }
-
+    
     @Override
     protected boolean configIsValid(@Nullable ColossalColosseumConfig config) throws IllegalArgumentException {
-        return false;
+        Preconditions.checkArgument(config != null, "Saved config is null");
+        Preconditions.checkArgument(config.version() != null, "version can't be null");
+        Preconditions.checkArgument(config.version().equals(Main.CONFIG_VERSION), "Config version %s not supported. %s required.", config.version(), Main.CONFIG_VERSION);
+        Preconditions.checkArgument(Bukkit.getWorld(config.world()) != null, "Could not find world \"%s\"", config.world());
+        Preconditions.checkArgument(config.firstPlaceSpawn() != null, "firstPlaceSpawn can't be null");
+        Preconditions.checkArgument(config.secondPlaceSpawn() != null, "secondPlaceSpawn can't be null");
+        Preconditions.checkArgument(config.spectatorSpawn() != null, "spectatorSpawn can't be null");
+        try {
+            GsonComponentSerializer.gson().deserializeFromTree(config.description());
+        } catch (JsonIOException | JsonSyntaxException e) {
+            throw new IllegalArgumentException("description is invalid", e);
+        }
+        return true;
     }
 
     @Override
