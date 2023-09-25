332f747f723a828f2004c64cd7dc214b6617d5f0
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-09-25 11:09:47 -0400

    added gates to config

--- src/main/java/org/braekpo1nt/mctmanager/games/colossalcolosseum/config/ColossalColosseumConfig.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/colossalcolosseum/config/ColossalColosseumConfig.java
@@ -4,7 +4,7 @@ import com.google.gson.JsonObject;
 import org.braekpo1nt.mctmanager.games.game.config.LocationDTO;
 
 record ColossalColosseumConfig(String version, String world, LocationDTO firstPlaceSpawn, LocationDTO secondPlaceSpawn, LocationDTO spectatorSpawn, int requiredWins, Gate firstPlaceGate, Gate secondPlaceGate, JsonObject description) {
-    record Gate(PosSize powder, PosSize stone) {
+    record Gate(PosSize clearArea, PosSize placeArea, PosSize stone) {
         record PosSize(int xOrigin, int yOrigin, int zOrigin, int xSize, int ySize, int zSize) {
         }
     }
