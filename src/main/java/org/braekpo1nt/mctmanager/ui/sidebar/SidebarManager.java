0098b49deee19d9200ca0a6286e6fdbc38a47e71
Braekpo1nt <braekpo1ntofficial@gmail.com>
2023-09-26 13:07:42 -0400

    added create and delete options

--- src/main/java/org/braekpo1nt/mctmanager/ui/sidebar/SidebarManager.java
+++ src/main/java/org/braekpo1nt/mctmanager/ui/sidebar/SidebarManager.java
@@ -1,9 +1,21 @@
 package org.braekpo1nt.mctmanager.ui.sidebar;
 
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class SidebarManager {
 
+    List<Sidebar> sidebars = new ArrayList<>();
     
+    public synchronized Sidebar createSidebar() {
+        Sidebar newSidebar = new Sidebar();
+        sidebars.add(newSidebar);
+        return newSidebar;
+    }
+    
+    public synchronized boolean deleteSidebar(Sidebar delete) {
+        return sidebars.remove(delete);
+    }
     
 }
