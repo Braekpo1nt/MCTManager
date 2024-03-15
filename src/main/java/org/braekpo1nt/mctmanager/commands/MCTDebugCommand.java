d6bd2b91d069903735b118199efe98227cfe1e2e
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-03-15 10:32:48 -0400

    displays a box in mctdebug

--- src/main/java/org/braekpo1nt/mctmanager/commands/MCTDebugCommand.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/MCTDebugCommand.java
@@ -3,22 +3,18 @@ package org.braekpo1nt.mctmanager.commands;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.braekpo1nt.mctmanager.Main;
-import org.bukkit.Bukkit;
-import org.bukkit.Color;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.BoundingBox;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -42,18 +38,34 @@ public class MCTDebugCommand implements TabExecutor {
                     .color(NamedTextColor.RED));
             return true;
         }
-    
-        if (args.length < 1) {
+        
+        if (args.length < 4) {
             sender.sendMessage(Component.text("Usage: /mctdebug <arg> [options]")
                     .color(NamedTextColor.RED));
             return true;
         }
-    
-        int index = Integer.parseInt(args[0]);
-        player.getInventory().setItem(EquipmentSlot.LEGS, new ItemStack(Material.STONE));
         
-        
-//        player.teleport(source.toLocation(player.getWorld(), yawPitch.yaw(), yawPitch.pitch()));
+        long ticks = Long.parseLong(args[0]); // 1 
+        int duration = Integer.parseInt(args[1]); // in ticks
+        int count = Integer.parseInt(args[2]); // 1
+        float size = Float.parseFloat(args[3]); // 1.0
+        BoundingBox box = new BoundingBox(
+                1, 1, 1,
+                10, 10, 10
+        );
+        List<Vector> hollowCube = createHollowCube(box);
+        new BukkitRunnable() {
+            int timeLeft = duration;
+            @Override
+            public void run() {
+                if (timeLeft <= 0) {
+                    this.cancel();
+                    return;
+                }
+                displayPoints(hollowCube, player, count, size);
+                timeLeft--;
+            }
+        }.runTaskTimerAsynchronously(plugin, 0L, ticks);
         
 //        Component mainTitle = Component.text("Main title");
 //        Component subTitle = Component.text("Subtitle");
@@ -62,6 +74,35 @@ public class MCTDebugCommand implements TabExecutor {
 //        Title title = Title.title(mainTitle, subTitle, times);
 //        sender.showTitle(title);
         return true;
+    }
+    
+    public static List<Vector> createHollowCube(BoundingBox area) {
+        List<Vector> result = new ArrayList<>();
+        int minX = area.getMin().getBlockX();
+        int minY = area.getMin().getBlockY();
+        int minZ = area.getMin().getBlockZ();
+        int maxX = area.getMax().getBlockX();
+        int maxY = area.getMax().getBlockY();
+        int maxZ = area.getMax().getBlockZ();
+        for (int x = minX; x <= maxX; x++) {
+            for (int y = minY; y <= maxY; y++) {
+                for (int z = minZ; z <= maxZ; z++) {
+                    if (
+                            x == minX || x == maxX
+                                    || y == minY || y == maxY
+                                    || z == minZ || z == maxZ) {
+                        result.add(new Vector(x, y, z));
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    
+    private void displayPoints(List<Vector> points, Player viewer, int count, float size) {
+        for (Vector v : points) {
+            viewer.spawnParticle(Particle.REDSTONE, v.getX(), v.getY(), v.getZ(), count, new Particle.DustOptions(Color.RED, size));
+        }
     }
     
     @Override
