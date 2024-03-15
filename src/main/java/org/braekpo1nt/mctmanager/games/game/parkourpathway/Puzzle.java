b82f6f8b4e69a7b64bf6a0d0a32ee38d884b40cc
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-03-15 10:32:35 -0400

    added comments

--- src/main/java/org/braekpo1nt/mctmanager/games/game/parkourpathway/Puzzle.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/parkourpathway/Puzzle.java
@@ -11,7 +11,8 @@ import java.util.List;
  *                 - all CheckPoint detectionAreas and respawns must be inside inBounds (otherwise players will constantly respawn to out-of-bounds areas)<br>
  *                 - the next puzzle's (or finish line's) CheckPoint detectionAreas and respawns must be inside inBounds (otherwise players will be teleported to the respawn point before they reach the next puzzle's checkpoint).<br> 
  *                 - Similarly, CheckPoint detectionAreas and respawns must be contained in the previous puzzle's inBounds area (or players won't reach this puzzle) <br>
- *                 - only the finish line (the last puzzle) doesn't need to contain the next puzzle's CheckPoint detectionAreas and respawns (because there isn't one)
+ *                 - only the finish line (the last puzzle) doesn't need to contain the next puzzle's CheckPoint detectionAreas and respawns (because there isn't one)<br>
+ *                 tl;dr: Must contain: all this Puzzle's checkpoints, all next Puzzle's checkpoints
  * @param checkPoints the list of checkpoints that players must reach to begin this puzzle. They contain the respawn point for if they go out of bounds, and the detectionArea which they must be inside to leave their previous puzzle and begin this one. Must have at least 1 element. Elements can't be null. detectionArea and respawn must be contained in the inBounds area. <br>
  *                    - CheckPoints can't have conflicting detectionAreas (meaning they can't overlap and can't have their respawns within another CheckPoint's detectionArea)
  */
