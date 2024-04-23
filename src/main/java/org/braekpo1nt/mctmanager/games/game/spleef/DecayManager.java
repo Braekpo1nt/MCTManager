96caf369f3f2032237290d12441ed9b8b889e71d
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-04-23 12:34:11 -0400

    refactored decaymanager

--- src/main/java/org/braekpo1nt/mctmanager/games/game/spleef/DecayManager.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/spleef/DecayManager.java
@@ -22,9 +22,28 @@ public class DecayManager {
     private final Main plugin;
     private final SpleefStorageUtil storageUtil;
     private final SpleefRound spleefRound;
+    private final Random random = new Random();
     private int decayTaskId;
+    /**
+     * the number of participants who are alive
+     */
     private long aliveCount;
+    /**
+     * the percent of participants who are alive
+     */
     private double alivePercent;
+    /**
+     * the index of the currently active stage
+     */
+    private int currentStageIndex;
+    /**
+     * the currently active stage
+     */
+    private DecayStage currentStage;
+    /**
+     * the seconds left in the stage
+     */
+    private int secondsLeft;
     
     public DecayManager(Main plugin, SpleefStorageUtil storageUtil, SpleefRound spleefRound) {
         this.plugin = plugin;
@@ -33,11 +52,17 @@ public class DecayManager {
     }
     
     public void start() {
+        currentStageIndex = 0;
+        currentStage = storageUtil.getStages().get(currentStageIndex);
+        secondsLeft = currentStage.getDuration();
         startDecayTask();
     }
     
     public void stop() {
         cancelAllTasks();
+        currentStageIndex = 0;
+        currentStage = null;
+        secondsLeft = 0;
     }
     
     private void cancelAllTasks() {
@@ -45,27 +70,14 @@ public class DecayManager {
     }
     
     private void startDecayTask() {
-        List<DecayStage> stages = storageUtil.getStages();
         this.decayTaskId = new BukkitRunnable() {
-            private final Random random = new Random();
-            private int currentStageIndex = 0;
-            private DecayStage currentStage = stages.get(currentStageIndex);
-            private int secondsLeft = currentStage.getDuration();
             @Override
             public void run() {
                 // if time is up, or num of living players is below threshold
                 // the final stage will continue on forever, regardless of the value of the duration or minParticipants
                 if ((secondsLeft <= 0 || aliveCount < currentStage.getMinParticipants()) || alivePercent < currentStage.getMinParticipantsPercent()) {
-                    // if this is not the final stage in the list
-                    if (currentStageIndex + 1 < stages.size()) {
-                        // move to the next stage
-                        currentStageIndex++;
-                        currentStage = stages.get(currentStageIndex);
-                        secondsLeft = currentStage.getDuration();
-                        if (currentStage.getStartMessage() != null) {
-                            spleefRound.messageAllParticipants(Component.text(currentStage.getStartMessage())
-                                    .color(NamedTextColor.DARK_RED));
-                        }
+                    if (hasNextStage()) {
+                        startNextStage();
                         return;
                     }
                     // otherwise, the final stage continues indefinitely
@@ -77,67 +89,87 @@ public class DecayManager {
                     decayLayer(decayLayer, layerInfo.blocksPerSecond());
                 }
             }
-            
-            /**
-             *
-             * @param decayLayer the area to decay within
-             * @param blocks the number of blocks to decay
-             */
-            private void decayLayer(BoundingBox decayLayer, int blocks) {
-                List<Block> coarseDirtBlocks = getCoarseDirtBlocks(decayLayer);
-                List<Block> dirtBlocks = getDirtBlocks(decayLayer);
-                
-                // Decay coarse dirt blocks to air
-                if (!coarseDirtBlocks.isEmpty()) {
-                    for (int i = 0; i < blocks; i++) {
-                        Block randomCoarseDirtBlock = coarseDirtBlocks.get(random.nextInt(coarseDirtBlocks.size()));
-                        randomCoarseDirtBlock.setType(Material.AIR);
-                    }
-                }
-                
-                // Decay dirt blocks to coarse dirt
-                if (!dirtBlocks.isEmpty()) {
-                    for (int i = 0; i < blocks; i++) {
-                        Block randomDirtBlock = dirtBlocks.get(random.nextInt(dirtBlocks.size()));
-                        randomDirtBlock.setType(storageUtil.getDecayBlock());
-                    }
-                }
-            }
-            
-            private List<Block> getDirtBlocks(BoundingBox layer) {
-                List<Block> dirtBlocks = new ArrayList<>();
-                
-                for (int x = layer.getMin().getBlockX(); x <= layer.getMaxX(); x++) {
-                    for (int y = layer.getMin().getBlockY(); y <= layer.getMaxY(); y++) {
-                        for (int z = layer.getMin().getBlockZ(); z <= layer.getMaxZ(); z++) {
-                            Block block = storageUtil.getWorld().getBlockAt(x, y, z);
-                            if (block.getType() == storageUtil.getLayerBlock()) {
-                                dirtBlocks.add(block);
-                            }
-                        }
-                    }
-                }
-                
-                return dirtBlocks;
-            }
-            
-            private List<Block> getCoarseDirtBlocks(BoundingBox layer) {
-                List<Block> coarseDirtBlocks = new ArrayList<>();
-                
-                for (int x = layer.getMin().getBlockX(); x <= layer.getMaxX(); x++) {
-                    for (int y = layer.getMin().getBlockY(); y <= layer.getMaxY(); y++) {
-                        for (int z = layer.getMin().getBlockZ(); z <= layer.getMaxZ(); z++) {
-                            Block block = storageUtil.getWorld().getBlockAt(x, y, z);
-                            if (block.getType() == storageUtil.getDecayBlock()) {
-                                coarseDirtBlocks.add(block);
-                            }
-                        }
-                    }
-                }
-                
-                return coarseDirtBlocks;
-            }
         }.runTaskTimer(plugin, 0L, 20L).getTaskId();
+    }
+    
+    /**
+     * @return true if there is a next stage, false if the current stage is the last stage
+     */
+    private boolean hasNextStage() {
+        return currentStageIndex + 1 < storageUtil.getStages().size();
+    }
+    
+    /**
+     * starts the next decay stage
+     */
+    private void startNextStage() {
+        currentStageIndex++;
+        currentStage = storageUtil.getStages().get(currentStageIndex);
+        secondsLeft = currentStage.getDuration();
+        if (currentStage.getStartMessage() != null) {
+            spleefRound.messageAllParticipants(Component.text(currentStage.getStartMessage())
+                    .color(NamedTextColor.DARK_RED));
+        }
+    }
+    
+    /**
+     * 
+     * @param decayLayer the area to decay within
+     * @param blocks the number of blocks to decay
+     */
+    private void decayLayer(BoundingBox decayLayer, int blocks) {
+        List<Block> coarseDirtBlocks = getCoarseDirtBlocks(decayLayer);
+        List<Block> dirtBlocks = getDirtBlocks(decayLayer);
+        
+        // Decay coarse dirt blocks to air
+        if (!coarseDirtBlocks.isEmpty()) {
+            for (int i = 0; i < blocks; i++) {
+                Block randomCoarseDirtBlock = coarseDirtBlocks.get(random.nextInt(coarseDirtBlocks.size()));
+                randomCoarseDirtBlock.setType(Material.AIR);
+            }
+        }
+        
+        // Decay dirt blocks to coarse dirt
+        if (!dirtBlocks.isEmpty()) {
+            for (int i = 0; i < blocks; i++) {
+                Block randomDirtBlock = dirtBlocks.get(random.nextInt(dirtBlocks.size()));
+                randomDirtBlock.setType(storageUtil.getDecayBlock());
+            }
+        }
+    }
+    
+    private List<Block> getDirtBlocks(BoundingBox layer) {
+        List<Block> dirtBlocks = new ArrayList<>();
+        
+        for (int x = layer.getMin().getBlockX(); x <= layer.getMaxX(); x++) {
+            for (int y = layer.getMin().getBlockY(); y <= layer.getMaxY(); y++) {
+                for (int z = layer.getMin().getBlockZ(); z <= layer.getMaxZ(); z++) {
+                    Block block = storageUtil.getWorld().getBlockAt(x, y, z);
+                    if (block.getType() == storageUtil.getLayerBlock()) {
+                        dirtBlocks.add(block);
+                    }
+                }
+            }
+        }
+        
+        return dirtBlocks;
+    }
+    
+    private List<Block> getCoarseDirtBlocks(BoundingBox layer) {
+        List<Block> coarseDirtBlocks = new ArrayList<>();
+        
+        for (int x = layer.getMin().getBlockX(); x <= layer.getMaxX(); x++) {
+            for (int y = layer.getMin().getBlockY(); y <= layer.getMaxY(); y++) {
+                for (int z = layer.getMin().getBlockZ(); z <= layer.getMaxZ(); z++) {
+                    Block block = storageUtil.getWorld().getBlockAt(x, y, z);
+                    if (block.getType() == storageUtil.getDecayBlock()) {
+                        coarseDirtBlocks.add(block);
+                    }
+                }
+            }
+        }
+        
+        return coarseDirtBlocks;
     }
     
     public void setAliveCount(long aliveCount) {
