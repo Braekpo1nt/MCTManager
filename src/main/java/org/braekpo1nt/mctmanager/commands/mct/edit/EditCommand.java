76c5393b47a41ae33218bcd4df538c395aca895b
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-05-02 15:40:50 -0400

    added remaining editor subcommands

--- src/main/java/org/braekpo1nt/mctmanager/commands/mct/edit/EditCommand.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/mct/edit/EditCommand.java
@@ -1,6 +1,9 @@
 package org.braekpo1nt.mctmanager.commands.mct.edit;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.braekpo1nt.mctmanager.commands.CommandUtils;
 import org.braekpo1nt.mctmanager.commands.commandmanager.CommandManager;
 import org.braekpo1nt.mctmanager.commands.commandmanager.SubCommand;
 import org.braekpo1nt.mctmanager.commands.commandmanager.commandresult.CommandResult;
@@ -30,40 +33,49 @@ public class EditCommand extends CommandManager {
                 return CommandResult.success();
             }
         });
-//        subCommands.put("save", (sender, command, label, args) -> {
-//            if (!gameManager.editorIsRunning()) {
-//                sender.sendMessage(Component.text("No editor is running.")
-//                        .color(NamedTextColor.RED));
-//                return true;
-//            }
-//            
-//            boolean force = false;
-//            if (args.length == 1) {
-//                String forceString = args[0];
-//                Boolean forceBoolean = CommandUtils.toBoolean(forceString);
-//                if (forceBoolean == null) {
-//                    sender.sendMessage(Component.empty()
-//                                    .append(Component.text(forceString)
-//                                            .decorate(TextDecoration.BOLD))
-//                                    .append(Component.text(" is not a valid boolean"))
-//                            .color(NamedTextColor.RED));
-//                    return true;
-//                }
-//                force = forceBoolean;
-//            }
-//            
-//            gameManager.saveEditor(sender, force);
-//            
-//            return true;
-//        });
-//        subCommands.put("load", (sender, command, label, args) -> {
-//            if (!gameManager.editorIsRunning()) {
-//                sender.sendMessage(Component.text("No editor is running.")
-//                        .color(NamedTextColor.RED));
-//                return true;
-//            }
-//            gameManager.loadEditor(sender);
-//            return true;
-//        });
+        addSubCommand(new SubCommand() {
+            @Override
+            public @NotNull String getName() {
+                return "save";
+            }
+    
+            @Override
+            public @NotNull CommandResult onSubCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+                if (!gameManager.editorIsRunning()) {
+                    return CommandResult.failure(Component.text("No editor is running."));
+                }
+    
+                boolean force = false;
+                if (args.length == 1) {
+                    String forceString = args[0];
+                    Boolean forceBoolean = CommandUtils.toBoolean(forceString);
+                    if (forceBoolean == null) {
+                        return CommandResult.failure(Component.empty()
+                                .append(Component.text(forceString)
+                                        .decorate(TextDecoration.BOLD))
+                                .append(Component.text(" is not a valid boolean")));
+                    }
+                    force = forceBoolean;
+                }
+    
+                gameManager.saveEditor(sender, force);
+                return CommandResult.success();
+            }
+        });
+        addSubCommand(new SubCommand() {
+            @Override
+            public @NotNull String getName() {
+                return "load";
+            }
+    
+            @Override
+            public @NotNull CommandResult onSubCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+                if (!gameManager.editorIsRunning()) {
+                    return CommandResult.failure(Component.text("No editor is running."));
+                }
+                gameManager.loadEditor(sender);
+                return CommandResult.success();
+            }
+        });
     }
 }
