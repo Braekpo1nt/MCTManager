998980abd8e30738730dd321b17d3ca3b35beb3b
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-05-02 15:23:55 -0400

    fixed GameCommand missing super()

--- src/main/java/org/braekpo1nt/mctmanager/commands/mct/game/GameCommand.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/mct/game/GameCommand.java
@@ -2,19 +2,14 @@ package org.braekpo1nt.mctmanager.commands.mct.game;
 
 import org.braekpo1nt.mctmanager.commands.commandmanager.CommandManager;
 import org.braekpo1nt.mctmanager.games.GameManager;
-import org.jetbrains.annotations.NotNull;
 
 public class GameCommand extends CommandManager {
     
     public GameCommand(GameManager gameManager) {
+        super("game");
         addSubCommand(new StartSubCommand(gameManager, "start"));
         addSubCommand(new StopSubCommand(gameManager, "stop"));
         addSubCommand(new VoteSubCommand(gameManager, "vote"));
         addSubCommand(new LoadSubCommand(gameManager, "load"));
-    }
-    
-    @Override
-    public @NotNull String getName() {
-        return "game";
     }
 }
