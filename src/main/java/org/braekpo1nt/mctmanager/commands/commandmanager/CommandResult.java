ff840e34ba352ce1d1da4d14770bffc174e864b2
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-05-02 10:04:52 -0400

    added some helper methods to CommandResult

--- src/main/java/org/braekpo1nt/mctmanager/commands/commandmanager/CommandResult.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/commandmanager/CommandResult.java
@@ -1,6 +1,7 @@
 package org.braekpo1nt.mctmanager.commands.commandmanager;
 
 import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public interface CommandResult {
@@ -8,13 +9,21 @@ public interface CommandResult {
     /**
      * @return the message to send to the command sender(s)
      */
-    Component getMessage();
+    @Nullable Component getMessage();
     
     static CommandResult succeeded(@Nullable Component message) {
         return new SuccessCommandResult(message);
     }
     
+    static CommandResult succeeded() {
+        return new SuccessCommandResult(null);
+    }
+    
     static CommandResult failed(@Nullable Component message) {
         return new FailureCommandResult(message);
+    }
+    
+    static CommandResult failed(@NotNull String message) {
+        return new FailureCommandResult(Component.text(message));
     }
 }
--- src/main/java/org/braekpo1nt/mctmanager/commands/commandmanager/FailureCommandResult.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/commandmanager/FailureCommandResult.java
@@ -2,6 +2,7 @@ package org.braekpo1nt.mctmanager.commands.commandmanager;
 
 import lombok.AllArgsConstructor;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 @AllArgsConstructor
 public class FailureCommandResult implements CommandResult {
@@ -9,6 +10,6 @@ public class FailureCommandResult implements CommandResult {
     
     @Override
     public Component getMessage() {
-        return message;
+        return message.color(NamedTextColor.RED);
     }
 }
