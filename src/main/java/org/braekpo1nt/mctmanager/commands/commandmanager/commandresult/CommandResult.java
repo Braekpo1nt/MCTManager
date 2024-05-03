1d124d646ebeffdca4755d3b84e2340abfe80b91
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-05-03 18:28:55 -0400

    added documentation to CommandResult

--- src/main/java/org/braekpo1nt/mctmanager/commands/commandmanager/commandresult/CommandResult.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/commandmanager/commandresult/CommandResult.java
@@ -12,24 +12,55 @@ public interface CommandResult {
      */
     @Nullable Component getMessage();
     
+    /**
+     * Concatenates an additional {@link CommandResult} after the current one.
+     * @param other the additional {@link CommandResult} to concatenate to this one.
+     * @return the combined {@link CommandResult}s
+     */
     @NotNull CommandResult and(CommandResult other);
     
-    static CommandResult success(@Nullable Component message) {
-        return new SuccessCommandResult(message);
-    }
-    
+    /**
+     * Indicates a successful use of a command.
+     * @return a new {@link SuccessCommandResult}
+     */
     static CommandResult success() {
         return new SuccessCommandResult(null);
     }
     
+    /**
+     * Indicates a successful use of a command, with an optional message for the sender.
+     * @param message the message to give the sender, if desired.
+     * @return a new {@link SuccessCommandResult}
+     */
+    static CommandResult success(@Nullable Component message) {
+        return new SuccessCommandResult(message);
+    }
+    
+    /**
+     * Indicates a failed use of a command, with an optional message for the sender.
+     * @param message a message indicating the nature of the failure.
+     * @return a new {@link FailureCommandResult}
+     */
     static CommandResult failure(@Nullable Component message) {
         return new FailureCommandResult(message);
     }
     
+    /**
+     * A convenience overload of {@link CommandResult#failure(Component)} where the message is a plain string.
+     * @param message a message indicating the nature of the failure. Can't be null.
+     * @return a new {@link FailureCommandResult}
+     * @see CommandResult#failure(Component)
+     */
     static CommandResult failure(@NotNull String message) {
         return new FailureCommandResult(Component.text(message));
     }
     
+    /**
+     * An overload of {@link CommandResult#failure(Component)} where the message is a {@link Usage} object.
+     * @param usage the {@link Usage} object describing the proper usage of the failed command
+     * @return a new {@link UsageCommandResult}
+     * @see CommandResult#failure(Component) 
+     */
     static CommandResult failure(@NotNull Usage usage) {
         return new UsageCommandResult(usage);
     }
