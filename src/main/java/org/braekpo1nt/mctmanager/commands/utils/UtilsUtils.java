e49d54dab5942f7c162488ae8a37f539b50dd3a0
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-05-07 11:32:47 -0400

    made less redundant and more generic

--- src/main/java/org/braekpo1nt/mctmanager/commands/utils/UtilsUtils.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/utils/UtilsUtils.java
@@ -54,41 +54,32 @@ class UtilsUtils {
     /**
      * Takes in an argument position and returns a coordinate value for tab completion.
      * The coordinate value returned is either the x, y, or z component of either the player's location or the block they are looking at. 
-     * Whether it is the x, y, or z component is determined by the argPos value (1 & 4 result in x; 2 & 5 result in y, 3 & 6 result in z). The "block" value will always be used (e.g. {@link Location#getBlockX()}).
+     * Whether it is the x, y, or z component is determined by the value of {@code argPos % 4} (0 results in an empty list; 1 results in x; 2 results in y, 3 results in z).
      * Whether it is from the player's location or the block they're targeting is determined by the distance between the player and the block they're looking at, as well as the material type of that block. If the player is looking at a material which is not in {@link UtilsUtils#TRANSPARENT} and is at most 5 blocks away, then the block position will be used. Otherwise, the player's location will be usd.
      * @param player the player to use as the location or targeted block location
      * @param argPos the position of the argument to tab complete (determines which component of the position to return, either x, y, or z)
-     * @return the appropriate coordinate component for the block the player is targeting or the player's location, depending on distance of and transparency of the targeted block.
+     * @return the appropriate coordinate component for the block the player is targeting or the player's location, depending on distance of and transparency of the targeted block. The "block" value will always be used (e.g. {@link Location#getBlockX()}).
      */
     public static @NotNull List<@NotNull String> tabCompleteCoordinates(@NotNull Player player, int argPos) {
-        if (argPos == 0 || argPos > 6) {
+        int m = argPos % 4;
+        if (m == 0) {
             return Collections.emptyList();
         }
         Block targetBlock = player.getTargetBlock(TRANSPARENT, 5);
+        Location location;
         if (TRANSPARENT.contains(targetBlock.getType())) {
-            Bukkit.getLogger().info(String.format("args: %s, player (%s, %s)", argPos, targetBlock.getType(), targetBlock.getLocation().toVector()));
-            switch (argPos) {
-                case 1, 4 -> {
-                    return Collections.singletonList("" + player.getLocation().getBlockX());
-                }
-                case 2, 5 -> {
-                    return Collections.singletonList("" + player.getLocation().getBlockY());
-                }
-                case 3, 6 -> {
-                    return Collections.singletonList("" + player.getLocation().getBlockZ());
-                }
-            }
+            location = player.getLocation();
+        } else {
+            location = targetBlock.getLocation();
         }
-        Bukkit.getLogger().info(String.format("args: %s, targetBlock (%s, %s)", argPos, targetBlock.getType(), targetBlock.getLocation().toVector()));
-        switch (argPos) {
-            case 1, 4 -> {
-                return Collections.singletonList("" + targetBlock.getLocation().getBlockX());
+        switch (m) {case 1 -> {
+                return Collections.singletonList("" + location.getBlockX());
             }
-            case 2, 5 -> {
-                return Collections.singletonList("" + targetBlock.getLocation().getBlockY());
+            case 2 -> {
+                return Collections.singletonList("" + location.getBlockY());
             }
-            case 3, 6 -> {
-                return Collections.singletonList("" + targetBlock.getLocation().getBlockZ());
+            case 3 -> {
+                return Collections.singletonList("" + location.getBlockZ());
             }
         }
         return Collections.emptyList();
