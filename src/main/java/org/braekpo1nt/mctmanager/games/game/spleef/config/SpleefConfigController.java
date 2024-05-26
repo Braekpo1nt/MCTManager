b2202814d510a7da6bd8e7787bf3c502be7ce8d5
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-05-25 22:43:58 -0400

    added saveConfig

--- src/main/java/org/braekpo1nt/mctmanager/games/game/spleef/config/SpleefConfigController.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/spleef/config/SpleefConfigController.java
@@ -6,6 +6,7 @@ import org.braekpo1nt.mctmanager.config.ConfigUtils;
 import org.braekpo1nt.mctmanager.config.exceptions.ConfigIOException;
 import org.braekpo1nt.mctmanager.config.exceptions.ConfigInvalidException;
 import org.braekpo1nt.mctmanager.config.validation.Validator;
+import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
@@ -53,6 +54,39 @@ public class SpleefConfigController {
             throw new ConfigIOException(String.format("Error while reading %s", configFile), e);
         } catch (JsonSyntaxException e) {
             throw new ConfigInvalidException(String.format("Error parsing %s", configFile), e);
+        }
+    }
+    
+    /**
+     * Saves the given Config to storage
+     * @param config the config to save
+     * @throws ConfigIOException if there is an IO error saving the config to the file system
+     */
+    private void saveConfigDTO(SpleefConfig config) {
+        try {
+            if (!configFile.exists()) {
+                if (!configFile.mkdirs()) {
+                    throw new ConfigIOException(String.format("Unable to create directories for %s", configFile));
+                }
+                if (configFile.createNewFile()) {
+                    throw new ConfigIOException(String.format("Unable to create config file %s", configFile));
+                }
+            }
+        } catch (SecurityException e) {
+            throw new ConfigIOException(String.format("Security exception while trying to save config to %s", configFile), e);
+        } catch (IOException e) {
+            throw new ConfigIOException(String.format("Error while trying to create %s", configFile), e);
+        }
+        
+        SpleefConfigDTO configDTO = SpleefConfigDTO.fromConfig(config);
+        
+        try {
+            Writer writer = new FileWriter(configFile, false);
+            ConfigUtils.GSON.toJson(configDTO, writer);
+            writer.flush();
+            writer.close();
+        } catch (JsonIOException | IOException e) {
+            throw new ConfigIOException(String.format("Error while writing %s", configFile), e);
         }
     }
     
