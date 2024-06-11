e20e6cae4697ec2e5eb4ffb50ffc9ae6dc083628
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-06-11 11:18:48 -0400

    implemented PresetLeave

--- src/main/java/org/braekpo1nt/mctmanager/commands/mct/team/preset/editor/PresetLeaveSubCommand.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/mct/team/preset/editor/PresetLeaveSubCommand.java
@@ -1,8 +1,13 @@
 package org.braekpo1nt.mctmanager.commands.mct.team.preset.editor;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.braekpo1nt.mctmanager.commands.manager.TabSubCommand;
 import org.braekpo1nt.mctmanager.commands.manager.commandresult.CommandResult;
+import org.braekpo1nt.mctmanager.config.exceptions.ConfigException;
+import org.braekpo1nt.mctmanager.games.gamestate.preset.Preset;
 import org.braekpo1nt.mctmanager.games.gamestate.preset.PresetController;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
@@ -25,7 +30,51 @@ public class PresetLeaveSubCommand extends TabSubCommand {
     
     @Override
     public @NotNull CommandResult onSubCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (args.length != 1) {
+            return CommandResult.failure(getUsage().of("<member>"));
+        }
+        String ign = args[0];
+        return leavePlayer(ign);
+    }
+    
+    private @NotNull CommandResult leavePlayer(@NotNull String ign) {
+        if (ign.isEmpty()) {
+            return CommandResult.failure("Player name can't be blank");
+        }
+        
+        Preset preset;
+        try {
+            preset = controller.getPreset();
+        } catch (ConfigException e) {
+            Bukkit.getLogger().severe(String.format("Could not load preset. %s", e.getMessage()));
+            e.printStackTrace();
+            return CommandResult.failure(Component.empty()
+                    .append(Component.text("Error occurred loading preset. See console for details: "))
+                    .append(Component.text(e.getMessage())));
+        }
+        if (!preset.hasMember(ign)) {
+            return CommandResult.failure(Component.empty()
+                    .append(Component.text(ign)
+                            .decorate(TextDecoration.BOLD))
+                    .append(Component.text(" is not in the preset"))
+            );
+        }
+        preset.leaveMember(ign);
+        try {
+            controller.savePreset(preset);
+        } catch (ConfigException e) {
+            Bukkit.getLogger().severe(String.format("Could not save preset. %s", e.getMessage()));
+            e.printStackTrace();
+            return CommandResult.failure(Component.empty()
+                    .append(Component.text("Error occurred saving preset. See console for details: "))
+                    .append(Component.text(e.getMessage())));
+        }
+        return CommandResult.success(Component.empty()
+                .append(Component.text("Left "))
+                .append(Component.text(ign)
+                        .decorate(TextDecoration.BOLD))
+                .append(Component.text(" from the preset"))
+        );
     }
     
     @Override
