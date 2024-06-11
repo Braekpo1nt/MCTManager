794257b9fd0b75b17415c4db6ca43fbcadcf3a10
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-06-11 09:36:26 -0400

    added basic add/remove leave/join

--- src/main/java/org/braekpo1nt/mctmanager/commands/mct/team/preset/PresetCommand.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/mct/team/preset/PresetCommand.java
@@ -1,6 +1,10 @@
 package org.braekpo1nt.mctmanager.commands.mct.team.preset;
 
 import org.braekpo1nt.mctmanager.commands.manager.CommandManager;
+import org.braekpo1nt.mctmanager.commands.mct.team.preset.editor.PresetAddSubCommand;
+import org.braekpo1nt.mctmanager.commands.mct.team.preset.editor.PresetJoinSubCommand;
+import org.braekpo1nt.mctmanager.commands.mct.team.preset.editor.PresetLeaveSubCommand;
+import org.braekpo1nt.mctmanager.commands.mct.team.preset.editor.PresetRemoveSubCommand;
 import org.braekpo1nt.mctmanager.games.GameManager;
 import org.braekpo1nt.mctmanager.games.gamestate.preset.PresetController;
 import org.jetbrains.annotations.NotNull;
@@ -16,5 +20,11 @@ public class PresetCommand extends CommandManager {
         this.controller = new PresetController(configDirectory);
         addSubCommand(new PresetApplySubCommand(gameManager, controller, "apply"));
         addSubCommand(new PresetWhitelistSubCommand(gameManager, controller, "whitelist"));
+        
+        // editor
+        addSubCommand(new PresetAddSubCommand(controller, "add"));
+        addSubCommand(new PresetRemoveSubCommand(controller, "remove"));
+        addSubCommand(new PresetJoinSubCommand(controller, "join"));
+        addSubCommand(new PresetLeaveSubCommand(controller, "leave"));
     }
 }
---
+++ src/main/java/org/braekpo1nt/mctmanager/commands/mct/team/preset/editor/PresetJoinSubCommand.java
@@ -0,0 +1,35 @@
+package org.braekpo1nt.mctmanager.commands.mct.team.preset.editor;
+
+import org.braekpo1nt.mctmanager.commands.manager.TabSubCommand;
+import org.braekpo1nt.mctmanager.commands.manager.commandresult.CommandResult;
+import org.braekpo1nt.mctmanager.games.gamestate.preset.PresetController;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+/**
+ * Lets you join a player to the preset by ign
+ */
+public class PresetJoinSubCommand extends TabSubCommand {
+    
+    
+    private final PresetController controller;
+    
+    public PresetJoinSubCommand(PresetController controller, @NotNull String name) {
+        super(name);
+        this.controller = controller;
+    }
+    
+    @Override
+    public @NotNull CommandResult onSubCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return null;
+    }
+    
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return List.of();
+    }
+}
