e6d56e3d79d9381b1c4648e99a868dda2320f6a3
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-06-13 14:21:23 -0400

    added MockGameManager and MockHubManager

--- src/test/java/org/braekpo1nt/mctmanager/MockMain.java
+++ src/test/java/org/braekpo1nt/mctmanager/MockMain.java
@@ -1,6 +1,7 @@
 package org.braekpo1nt.mctmanager;
 
 import org.braekpo1nt.mctmanager.games.GameManager;
+import org.braekpo1nt.mctmanager.games.MockGameManager;
 import org.braekpo1nt.mctmanager.games.gamestate.MockGameStateStorageUtil;
 import org.braekpo1nt.mctmanager.ui.sidebar.MockSidebarFactory;
 import org.bukkit.scoreboard.Scoreboard;
@@ -9,7 +10,7 @@ public class MockMain extends Main {
     
     @Override
     protected GameManager initialGameManager(Scoreboard mctScoreboard) {
-        GameManager gameManager = new GameManager(this, mctScoreboard);
+        GameManager gameManager = new MockGameManager(this, mctScoreboard);
         MockGameStateStorageUtil mockGameStateStorageUtil = new MockGameStateStorageUtil(this);
         gameManager.setGameStateStorageUtil(mockGameStateStorageUtil);
         MockSidebarFactory mockSidebarFactory = new MockSidebarFactory();
---
+++ src/test/java/org/braekpo1nt/mctmanager/hub/MockHubManager.java
@@ -0,0 +1,11 @@
+package org.braekpo1nt.mctmanager.hub;
+
+import org.braekpo1nt.mctmanager.Main;
+import org.braekpo1nt.mctmanager.games.GameManager;
+
+public class MockHubManager extends HubManager {
+    
+    public MockHubManager(Main plugin, GameManager gameManager) {
+        super(plugin, gameManager);
+    }
+}
