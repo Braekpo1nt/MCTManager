626d888403f6cd7c8b86f685b0c804c2c6d1fa6f
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-06-13 12:26:27 -0400

    mocked initialGameManager

--- src/test/java/org/braekpo1nt/mctmanager/MockMain.java
+++ src/test/java/org/braekpo1nt/mctmanager/MockMain.java
@@ -1,4 +1,19 @@
 package org.braekpo1nt.mctmanager;
 
+import org.braekpo1nt.mctmanager.games.GameManager;
+import org.braekpo1nt.mctmanager.games.gamestate.MockGameStateStorageUtil;
+import org.braekpo1nt.mctmanager.ui.sidebar.MockSidebarFactory;
+import org.bukkit.scoreboard.Scoreboard;
+
 public class MockMain extends Main {
+    
+    @Override
+    protected GameManager initialGameManager(Scoreboard mctScoreboard) {
+        GameManager gameManager = new GameManager(this, mctScoreboard);
+        MockGameStateStorageUtil mockGameStateStorageUtil = new MockGameStateStorageUtil(this);
+        gameManager.setGameStateStorageUtil(mockGameStateStorageUtil);
+        MockSidebarFactory mockSidebarFactory = new MockSidebarFactory();
+        gameManager.setSidebarFactory(mockSidebarFactory);
+        return gameManager;
+    }
 }
