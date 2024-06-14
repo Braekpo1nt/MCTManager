ce3d554b486e431b53aecfbddfb3ba479bdb0ffc
Braekpo1nt <94723887+Braekpo1nt@users.noreply.github.com>
2024-06-14 16:27:56 -0400

    490 keep players from walking off platform (#499)
    * made footrace reflect new version of config
    
    * added example preventInteractions to every example config of 0.1.1
    
    * added geometry
    
    * added safetyArea to config
    
    * fixed safetyArea bugs

---
+++ src/main/java/org/braekpo1nt/mctmanager/config/dto/org/braekpo1nt/mctmanager/geometry/GeometryDeserializer.java
@@ -0,0 +1,28 @@
+package org.braekpo1nt.mctmanager.config.dto.org.braekpo1nt.mctmanager.geometry;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonDeserializer;
+import org.braekpo1nt.mctmanager.geometry.BoundingCylinder;
+import org.braekpo1nt.mctmanager.geometry.BoundingRectangle;
+import org.braekpo1nt.mctmanager.geometry.Geometry;
+
+import java.lang.reflect.Type;
+
+public class GeometryDeserializer implements JsonDeserializer<Geometry> {
+    @Override
+    public Geometry deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+        JsonElement centerX = jsonObject.get("centerX");
+        if (centerX != null) {
+            return context.deserialize(json, BoundingCylinder.class);
+        }
+        JsonElement minX = jsonObject.get("minX");
+        if (minX != null) {
+            return context.deserialize(json, BoundingRectangle.class);
+        }
+        throw new JsonParseException("Unknown geometry object: " + jsonObject.getAsString());
+    }
+}
