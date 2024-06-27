7ef311808a3737e20c40901ca75268720c11c701
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-06-27 12:21:17 -0400

    returned MCTDebugCOmmand to original state

--- src/main/java/org/braekpo1nt/mctmanager/commands/mctdebug/MCTDebugCommand.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/mctdebug/MCTDebugCommand.java
@@ -3,16 +3,11 @@ package org.braekpo1nt.mctmanager.commands.mctdebug;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.braekpo1nt.mctmanager.Main;
-import org.braekpo1nt.mctmanager.commands.CommandUtils;
-import org.braekpo1nt.mctmanager.games.GameManager;
-import org.braekpo1nt.mctmanager.ui.topbar.Topbar;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -23,8 +18,6 @@ import java.util.List;
  * A utility command for testing various things, so I don't have to create a new command. 
  */
 public class MCTDebugCommand implements TabExecutor, Listener {
-    
-    private final Topbar topbar = new Topbar();
     
     public MCTDebugCommand(Main plugin) {
         plugin.getCommand("mctdebug").setExecutor(this);
@@ -44,15 +37,6 @@ public class MCTDebugCommand implements TabExecutor, Listener {
             sender.sendMessage(Component.text("Usage: /mctdebug <arg> [options]")
                     .color(NamedTextColor.RED));
             return true;
-        }
-        
-        for (ItemStack item : player.getInventory().getContents()) {
-            if (item != null) {
-                if (item.getItemMeta() instanceof LeatherArmorMeta leatherArmorMeta) {
-                    leatherArmorMeta.setColor(null);
-                    item.setItemMeta(leatherArmorMeta);
-                }
-            }
         }
         
 //        Component mainTitle = Component.text("Main title");
