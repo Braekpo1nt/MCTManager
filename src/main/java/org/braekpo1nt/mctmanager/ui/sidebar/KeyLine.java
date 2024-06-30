5608818868207cf2fb170f20300e380d8a972dda
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-06-30 16:01:02 -0400

    made keyline use Components

--- src/main/java/org/braekpo1nt/mctmanager/ui/sidebar/KeyLine.java
+++ src/main/java/org/braekpo1nt/mctmanager/ui/sidebar/KeyLine.java
@@ -1,14 +1,18 @@
 package org.braekpo1nt.mctmanager.ui.sidebar;
 
 import lombok.Data;
+import net.kyori.adventure.text.Component;
 import org.jetbrains.annotations.NotNull;
 
 
 @Data
 public class KeyLine {
     private final @NotNull String key;
-    private final @NotNull String contents;
+    private final @NotNull Component contents;
     
-    
+    public KeyLine(@NotNull String key, @NotNull String contents) {
+        this.key = key;
+        this.contents = Component.text(contents);
+    }
     
 }
