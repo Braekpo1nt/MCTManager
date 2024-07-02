f763e5baed6d72a3e3b5e99aefc7d44dd06548e0
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-07-02 13:28:30 -0400

    added tab completers to timer command

--- src/main/java/org/braekpo1nt/mctmanager/commands/mct/timer/TimerCommand.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/mct/timer/TimerCommand.java
@@ -2,34 +2,53 @@ package org.braekpo1nt.mctmanager.commands.mct.timer;
 
 import org.braekpo1nt.mctmanager.commands.manager.CommandManager;
 import org.braekpo1nt.mctmanager.commands.manager.SubCommand;
+import org.braekpo1nt.mctmanager.commands.manager.TabSubCommand;
 import org.braekpo1nt.mctmanager.commands.manager.commandresult.CommandResult;
 import org.braekpo1nt.mctmanager.games.GameManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class TimerCommand extends CommandManager {
     public TimerCommand(@NotNull GameManager gameManager, @NotNull String name) {
         super(name);
-        addSubCommand(new SubCommand("pause") {
+        addSubCommand(new TabSubCommand("pause") {
             @Override
             public @NotNull CommandResult onSubCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
                 gameManager.getTimerManager().pause();
                 return CommandResult.success();
             }
+            
+            @Override
+            public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+                return List.of();
+            }
         });
-        addSubCommand(new SubCommand("resume") {
+        addSubCommand(new TabSubCommand("resume") {
             @Override
             public @NotNull CommandResult onSubCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
                 gameManager.getTimerManager().resume();
                 return CommandResult.success();
             }
+            
+            @Override
+            public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+                return List.of();
+            }
         });
-        addSubCommand(new SubCommand("skip") {
+        addSubCommand(new TabSubCommand("skip") {
             @Override
             public @NotNull CommandResult onSubCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
                 gameManager.getTimerManager().skip();
                 return CommandResult.success();
+            }
+            
+            @Override
+            public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+                return List.of();
             }
         });
     }
