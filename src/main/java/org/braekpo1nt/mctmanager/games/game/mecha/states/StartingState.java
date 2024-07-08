d072d5e8f5735148cf092fac23366bd23dfd77ab
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-07-08 11:51:47 -0400

    initially setup state pattern

---
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/mecha/states/StartingState.java
@@ -0,0 +1,10 @@
+package org.braekpo1nt.mctmanager.games.game.mecha.states;
+
+import lombok.AllArgsConstructor;
+import org.braekpo1nt.mctmanager.games.game.mecha.MechaGame;
+
+@AllArgsConstructor
+public class StartingState implements MechaState {
+    private MechaGame context;
+    
+}
