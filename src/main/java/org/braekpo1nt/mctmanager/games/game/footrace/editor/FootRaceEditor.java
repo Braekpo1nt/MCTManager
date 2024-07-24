7834acdef1fe321df7e06c4cce3e7fa02e6622b6
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-07-24 11:21:51 -0400

    added addRemoveCheckpointWand

--- src/main/java/org/braekpo1nt/mctmanager/games/game/footrace/editor/FootRaceEditor.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/footrace/editor/FootRaceEditor.java
@@ -2,6 +2,7 @@ package org.braekpo1nt.mctmanager.games.game.footrace.editor;
 
 import com.google.common.base.Preconditions;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.braekpo1nt.mctmanager.Main;
 import org.braekpo1nt.mctmanager.config.exceptions.ConfigIOException;
 import org.braekpo1nt.mctmanager.config.exceptions.ConfigInvalidException;
@@ -14,15 +15,22 @@ import org.braekpo1nt.mctmanager.games.game.footrace.config.FootRaceConfig;
 import org.braekpo1nt.mctmanager.games.game.footrace.config.FootRaceConfigController;
 import org.braekpo1nt.mctmanager.games.game.interfaces.Configurable;
 import org.braekpo1nt.mctmanager.games.game.interfaces.GameEditor;
-import org.braekpo1nt.mctmanager.games.game.parkourpathway.puzzle.Puzzle;
+import org.braekpo1nt.mctmanager.games.utils.GameManagerUtils;
 import org.braekpo1nt.mctmanager.games.utils.ParticipantInitializer;
 import org.braekpo1nt.mctmanager.ui.sidebar.KeyLine;
 import org.braekpo1nt.mctmanager.ui.sidebar.Sidebar;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.BoundingBox;
 import org.bukkit.util.Vector;
@@ -39,6 +47,7 @@ public class FootRaceEditor implements GameEditor, Configurable, Listener {
     private List<Player> participants;
     private Map<UUID, Display> displays;
     // wands
+    private final ItemStack addRemoveCheckpointWand;
     private final List<ItemStack> allWands = new ArrayList<>();
     // end wands
     /**
@@ -51,6 +60,29 @@ public class FootRaceEditor implements GameEditor, Configurable, Listener {
         this.plugin = plugin;
         this.gameManager = gameManager;
         this.controller = new FootRaceConfigController(plugin.getDataFolder());
+        this.addRemoveCheckpointWand = addWand(
+                Component.text("Add/Remove Checkpoint"),
+                List.of(
+                        Component.text("Left Click: add checkpoint"),
+                        Component.text("Right Click: remove checkpoint")
+                )
+        );
+    }
+    
+    /**
+     * add a wand to the editor, with the given name and lore. Adds it to the list of all wands.
+     * @param name the display name of the wand
+     * @param lore the description of the wand
+     * @return the new wand
+     */
+    private ItemStack addWand(Component name, List<Component> lore) {
+        ItemStack newWand = new ItemStack(Material.STICK);
+        newWand.editMeta(meta -> {
+            meta.displayName(name);
+            meta.lore(lore);
+        });
+        allWands.add(newWand);
+        return newWand;
     }
     
     @Override
@@ -163,7 +195,7 @@ public class FootRaceEditor implements GameEditor, Configurable, Listener {
     
     @Override
     public GameType getType() {
-        return null;
+        return GameType.FOOT_RACE;
     }
     
     @Override
@@ -189,6 +221,98 @@ public class FootRaceEditor implements GameEditor, Configurable, Listener {
                     false
             );
         }
+    }
+    
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player participant = event.getPlayer();
+        ItemStack item = event.getItem();
+        if (item == null) {
+            return;
+        }
+        if (!allWands.contains(item)) {
+            return;
+        }
+        event.setCancelled(true);
+        Action action = event.getAction();
+        if (item.equals(addRemoveCheckpointWand)) {
+            useInAddRemoveCheckpointWand(participant, action);
+        }
+    }
+    
+    private void useInAddRemoveCheckpointWand(Player participant, Action action) {
+        int currentCheckpointIndex = currentCheckpoints.get(participant.getUniqueId());
+        switch (action) {
+            case LEFT_CLICK_AIR, LEFT_CLICK_BLOCK -> {
+                int newCheckpointIndex = currentCheckpointIndex + 1;
+                participant.sendMessage(Component.text("Add checkpoint index ")
+                        .append(Component.text(newCheckpointIndex))
+                        .append(Component.text("/"))
+                        .append(Component.text(config.getCheckpoints().size()))
+                        .append(Component.text(". Max index is now "))
+                        .append(Component.text(config.getCheckpoints().size() + 1))
+                );
+                BoundingBox checkpoint = createCheckpoint(participant.getLocation());
+                config.getCheckpoints().add(newCheckpointIndex, checkpoint);
+                selectCheckpoint(participant, newCheckpointIndex, false);
+            }
+            case RIGHT_CLICK_AIR, RIGHT_CLICK_BLOCK -> {
+                if (config.getCheckpoints().size() == 3) {
+                    participant.sendMessage(Component.text("There must be at least 3 checkpoints")
+                            .color(NamedTextColor.RED));
+                    return;
+                }
+                participant.sendMessage(Component.text("Remove checkpoint index ")
+                        .append(Component.text(currentCheckpointIndex))
+                        .append(Component.text("/"))
+                        .append(Component.text(config.getCheckpoints().size()))
+                        .append(Component.text(". Max index is now "))
+                        .append(Component.text(config.getCheckpoints().size() - 1))
+                );
+                config.getCheckpoints().remove(currentCheckpointIndex);
+                selectCheckpoint(participant, currentCheckpointIndex - 1, false);
+            }
+        }
+    }
+    
+    /**
+     * Create a new Checkpoint in the given position
+     * @param location the position of the min corner of the checkpoint
+     * @return a new checkpoint
+     */
+    private BoundingBox createCheckpoint(Location location) {
+        Location p = location.toBlockLocation();
+        return new BoundingBox(
+                p.getX(),
+                p.getY(),
+                p.getZ(),
+                p.getX() + 2,
+                p.getY() + 2,
+                p.getZ() + 2
+        );
+    }
+    
+    @EventHandler
+    public void onClickInventory(InventoryClickEvent event) {
+        Player participant = ((Player) event.getWhoClicked());
+        if (!participants.contains(participant)) {
+            return;
+        }
+        if (!GameManagerUtils.INV_REMOVE_ACTIONS.contains(event.getAction())) {
+            return;
+        }
+        event.setCancelled(true);
+    }
+    
+    @EventHandler
+    public void onDropItem(PlayerDropItemEvent event) {
+        if (!participants.contains(event.getPlayer())) {
+            return;
+        }
+        if (!allWands.contains(event.getItemDrop().getItemStack())) {
+            return;
+        }
+        event.setCancelled(true);
     }
     
     private void initializeSidebar() {
