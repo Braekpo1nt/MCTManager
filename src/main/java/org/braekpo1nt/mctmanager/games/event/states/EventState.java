e3382873cb2ebe91bf998cb4d6dd5f961566ae52
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-07-26 23:21:35 -0400

    wip: added public methods

--- src/main/java/org/braekpo1nt/mctmanager/games/event/states/EventState.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/event/states/EventState.java
@@ -1,8 +1,22 @@
 package org.braekpo1nt.mctmanager.games.event.states;
 
+import org.braekpo1nt.mctmanager.games.game.enums.GameType;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.jetbrains.annotations.NotNull;
 
 public interface EventState {
     void onParticipantJoin(Player participant);
     void onParticipantQuit(Player participant);
+    void onAdminJoin(Player admin);
+    void onAdminQuit(Player admin);
+    void startEvent(CommandSender sender, int numberOfGames);
+    void stopEvent(CommandSender sender);
+    void undoGame(@NotNull CommandSender sender, @NotNull GameType gameType, int iterationIndex);
+    void onPlayerDamage(EntityDamageEvent event);
+    void onClickInventory(InventoryClickEvent event);
+    void onDropItem(PlayerDropItemEvent event);
 }
