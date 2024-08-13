ea7360a4330097c133873e8af6f070cd8e8bddd8
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-08-13 11:41:36 -0400

    fixed comment being one long line

--- src/main/java/org/braekpo1nt/mctmanager/games/colossalcombat/config/ColossalCombatConfigDTO.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/colossalcombat/config/ColossalCombatConfigDTO.java
@@ -264,7 +264,10 @@ record ColossalCombatConfigDTO(
     
     /**
      * @param roundStarting the duration (in seconds) to count down before the gates drop and the match starts
-     * @param antiSuffocation the duration (in ticks) to prevent players from walking over the area that would cause them to suffocate in the concrete powder wall as the blocks fall. Careful, if this is not long enough the players will suffocate, and if it's too long they'll get frustrated. TODO: implement a more automated version of this. 
+     * @param antiSuffocation the duration (in ticks) to prevent players from walking over the area that would cause 
+     *                        them to suffocate in the concrete powder wall as the blocks fall. Careful, if this is not 
+     *                        long enough the players will suffocate, and if it's too long they'll get frustrated. 
+     *                        TODO: implement a more automated version of this. 
      */
     record Durations(int roundStarting, long antiSuffocation, int description) {
     }
