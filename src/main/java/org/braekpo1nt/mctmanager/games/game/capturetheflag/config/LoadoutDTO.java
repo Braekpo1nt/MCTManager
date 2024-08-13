04cdb5014727ab80efe04e1cc8cfbfc5ee9233f9
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-08-13 16:58:57 -0400

    removed debug statement

--- src/main/java/org/braekpo1nt/mctmanager/games/game/capturetheflag/config/LoadoutDTO.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/capturetheflag/config/LoadoutDTO.java
@@ -8,7 +8,6 @@ import org.braekpo1nt.mctmanager.config.dto.org.bukkit.inventory.meta.ItemMetaDT
 import org.braekpo1nt.mctmanager.config.validation.Validatable;
 import org.braekpo1nt.mctmanager.config.validation.Validator;
 import org.braekpo1nt.mctmanager.games.game.capturetheflag.Loadout;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -51,7 +50,6 @@ class LoadoutDTO implements Validatable {
         validator.notNull(menuItem, "menuItem");
         validator.notNull(menuLore, "lore");
         if (menuMeta != null) {
-            Bukkit.getLogger().info("validating menu meta");
             menuMeta.validate(validator.path("menuMeta"));
         }
         validator.notNull(inventory, "inventory");
