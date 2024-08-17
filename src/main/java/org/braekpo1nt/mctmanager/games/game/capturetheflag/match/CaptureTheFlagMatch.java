3589fde74c1643850b2983834db673d8279eca0d
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-08-17 12:14:53 -0400

    implemented stub of match

--- src/main/java/org/braekpo1nt/mctmanager/games/game/capturetheflag/match/CaptureTheFlagMatch.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/capturetheflag/match/CaptureTheFlagMatch.java
@@ -1,4 +1,35 @@
 package org.braekpo1nt.mctmanager.games.game.capturetheflag.match;
 
+import lombok.Data;
+import org.braekpo1nt.mctmanager.games.game.capturetheflag.Arena;
+import org.braekpo1nt.mctmanager.games.game.capturetheflag.CaptureTheFlagGame;
+import org.braekpo1nt.mctmanager.games.game.capturetheflag.MatchPairing;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+@Data
 public class CaptureTheFlagMatch {
+    
+    
+    private final CaptureTheFlagGame parentContext;
+    /**
+     * To be called when this match is over
+     */
+    private final Consumer<CaptureTheFlagMatch> matchIsOver;
+    private final MatchPairing matchPairing;
+    private final Arena arena;
+    
+    public CaptureTheFlagMatch(CaptureTheFlagGame parentContext, Consumer<CaptureTheFlagMatch> matchIsOver, MatchPairing matchPairing, Arena arena) {
+        this.parentContext = parentContext;
+        this.matchIsOver = matchIsOver;
+        this.matchPairing = matchPairing;
+        this.arena = arena;
+    }
+    
+    public void start(List<Player> northParticipants, List<Player> southParticipants) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+    
 }
--- src/main/java/org/braekpo1nt/mctmanager/games/game/capturetheflag/states/RoundActiveState.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/capturetheflag/states/RoundActiveState.java
@@ -1,18 +1,59 @@
 package org.braekpo1nt.mctmanager.games.game.capturetheflag.states;
 
+import org.braekpo1nt.mctmanager.games.GameManager;
 import org.braekpo1nt.mctmanager.games.game.capturetheflag.CaptureTheFlagGame;
+import org.braekpo1nt.mctmanager.games.game.capturetheflag.MatchPairing;
+import org.braekpo1nt.mctmanager.games.game.capturetheflag.RoundManager;
+import org.braekpo1nt.mctmanager.games.game.capturetheflag.match.CaptureTheFlagMatch;
+import org.braekpo1nt.mctmanager.ui.sidebar.Sidebar;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RoundActiveState implements CaptureTheFlagState {
     
     private final CaptureTheFlagGame context;
+    private final GameManager gameManager;
+    private final RoundManager roundManger;
+    private final List<Player> onDeckParticipants = new ArrayList<>();
+    private final List<CaptureTheFlagMatch> matches;
+    private int numOfEndedMatches = 0;
     
     public RoundActiveState(CaptureTheFlagGame context) {
         this.context = context;
+        this.gameManager = context.getGameManager();
+        this.roundManger = context.getRoundManager();
+        Sidebar sidebar = context.getSidebar();
+        Sidebar adminSidebar = context.getAdminSidebar();
+        
+        List<MatchPairing> currentRound = roundManger.getCurrentRound();
+        matches = new ArrayList<>(currentRound.size());
+        for (int i = 0; i < currentRound.size(); i++) {
+            MatchPairing matchPairing = currentRound.get(i);
+            matches.add(new CaptureTheFlagMatch(context, this::matchIsOver, matchPairing, context.getConfig().getArenas().get(i)));
+        }
+        
+        // TODO: call each match's start() method
+    }
+    
+    /**
+     * Called by each match when it ends, so that we know each one has ended
+     * @param endedMatch the match that ended
+     */
+    public void matchIsOver(CaptureTheFlagMatch endedMatch) {
+        numOfEndedMatches++;
+        if (numOfEndedMatches >= matches.size()) {
+            allMatchesAreOver();
+        }
+    }
+    
+    private void allMatchesAreOver() {
+        context.setState(new RoundOverState(context));
     }
     
     @Override
