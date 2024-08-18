cfa3e813aa6302d29b72a34d5c93c156eae3f12b
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-08-18 16:03:31 -0400

    implemented ClassSelection constructor

--- src/main/java/org/braekpo1nt/mctmanager/games/game/capturetheflag/match/states/ClassSelectionState.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/capturetheflag/match/states/ClassSelectionState.java
@@ -1,6 +1,10 @@
 package org.braekpo1nt.mctmanager.games.game.capturetheflag.match.states;
 
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import org.braekpo1nt.mctmanager.games.game.capturetheflag.ClassPicker;
 import org.braekpo1nt.mctmanager.games.game.capturetheflag.match.CaptureTheFlagMatch;
+import org.braekpo1nt.mctmanager.ui.timer.Timer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
@@ -11,9 +15,33 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class ClassSelectionState implements CaptureTheFlagMatchState {
     
     private final CaptureTheFlagMatch context;
+    private final ClassPicker northClassPicker;
+    private final ClassPicker southClassPicker;
     
     public ClassSelectionState(CaptureTheFlagMatch context) {
         this.context = context;
+        this.northClassPicker = context.getNorthClassPicker();
+        this.southClassPicker = context.getSouthClassPicker();
+        northClassPicker.start(
+                context.getPlugin(), 
+                context.getNorthParticipants(), 
+                context.getConfig().getLoadouts());
+        southClassPicker.start(
+                context.getPlugin(), 
+                context.getSouthParticipants(), 
+                context.getConfig().getLoadouts());
+        context.getTimerManager().start(Timer.builder()
+                .duration(context.getConfig().getClassSelectionDuration())
+                .withSidebar(context.getAdminSidebar(), "timer")
+                .withTopbar(context.getTopbar())
+                .sidebarPrefix(Component.text("Class selection: "))
+                .titleAudience(Audience.audience(context.getAllParticipants()))
+                .onCompletion(() -> {
+                    northClassPicker.stop(true);
+                    southClassPicker.stop(true);
+                    context.setState(new MatchActiveState(context));
+                })
+                .build());
     }
     
     @Override
