4d05bc127347fe46f6938e128665c8d7cb9704b3
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-08-31 11:42:19 -0400

    added FarmRushGame stub

---
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/farmrush/FarmRushGame.java
@@ -0,0 +1,77 @@
+package org.braekpo1nt.mctmanager.games.game.farmrush;
+
+import net.kyori.adventure.text.Component;
+import org.braekpo1nt.mctmanager.config.exceptions.ConfigIOException;
+import org.braekpo1nt.mctmanager.config.exceptions.ConfigInvalidException;
+import org.braekpo1nt.mctmanager.games.game.enums.GameType;
+import org.braekpo1nt.mctmanager.games.game.interfaces.Configurable;
+import org.braekpo1nt.mctmanager.games.game.interfaces.MCTGame;
+import org.braekpo1nt.mctmanager.ui.sidebar.Headerable;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class FarmRushGame implements MCTGame, Configurable, Headerable, Listener {
+    
+    @Override
+    public void loadConfig() throws ConfigIOException, ConfigInvalidException {
+        
+    }
+    
+    @Override
+    public GameType getType() {
+        return null;
+    }
+    
+    @Override
+    public void start(List<Player> newParticipants, List<Player> newAdmins) {
+        
+    }
+    
+    @Override
+    public void stop() {
+        
+    }
+    
+    @Override
+    public void onParticipantJoin(Player participant) {
+        
+    }
+    
+    @Override
+    public void onParticipantQuit(Player participant) {
+        
+    }
+    
+    @Override
+    public void onAdminJoin(Player admin) {
+        
+    }
+    
+    @Override
+    public void onAdminQuit(Player admin) {
+        
+    }
+    
+    @Override
+    public @NotNull Component getBaseTitle() {
+        return null;
+    }
+    
+    @Override
+    public void setTitle(@NotNull Component title) {
+        
+    }
+    
+    @Override
+    public void updatePersonalScore(Player participant, Component contents) {
+        
+    }
+    
+    @Override
+    public void updateTeamScore(Player participant, Component contents) {
+        
+    }
+}
