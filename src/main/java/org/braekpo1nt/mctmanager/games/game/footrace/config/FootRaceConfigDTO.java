f492b6161eed3532402b638db828c688fa393e3c
Braekpo1nt <94723887+Braekpo1nt@users.noreply.github.com>
2024-09-01 11:42:50 -0400

    595 replace boundingboxdto with adapter (#596)
    * added BoundingBoxDeserializer
    
    * replaced use of toBoundingBox()
    
    * cleared out use of BoundingBoxDTo.from()

--- src/main/java/org/braekpo1nt/mctmanager/games/game/footrace/config/FootRaceConfigDTO.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/footrace/config/FootRaceConfigDTO.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import net.kyori.adventure.text.Component;
 import org.braekpo1nt.mctmanager.Main;
 import org.braekpo1nt.mctmanager.config.dto.org.bukkit.LocationDTO;
-import org.braekpo1nt.mctmanager.config.dto.org.bukkit.util.BoundingBoxDTO;
 import org.braekpo1nt.mctmanager.config.validation.Validatable;
 import org.braekpo1nt.mctmanager.config.validation.Validator;
 import org.bukkit.Bukkit;
@@ -24,10 +23,10 @@ record FootRaceConfigDTO(
         String version, 
         String world, 
         LocationDTO startingLocation, 
-        @Nullable BoundingBoxDTO finishLine,
-        @Nullable List<BoundingBoxDTO> checkpoints,
-        @Nullable BoundingBoxDTO spectatorArea, 
-        BoundingBoxDTO glassBarrier,
+        @Nullable BoundingBox finishLine,
+        @Nullable List<BoundingBox> checkpoints,
+        @Nullable BoundingBox spectatorArea, 
+        BoundingBox glassBarrier,
         @Nullable List<Material> preventInteractions,
         Scores scores, 
         Durations durations, 
@@ -42,16 +41,14 @@ record FootRaceConfigDTO(
         validator.notNull(this.startingLocation(), "startingLocation");
         if (this.checkpoints == null) {
             validator.validate(this.finishLine != null, "finishLine can't be null if checkpoints is null");
-            BoundingBox finishLine = this.finishLine.toBoundingBox();
-            validator.validate(finishLine.getVolume() >= 1.0, "finishLine's volume (%s) can't be less than 1. %s", finishLine.getVolume(), finishLine);
+            validator.validate(this.finishLine.getVolume() >= 1.0, "finishLine's volume (%s) can't be less than 1. %s", finishLine.getVolume(), finishLine);
             validator.validate(!finishLine.contains(this.startingLocation().toVector()), "startingLocation (%S) can't be inside finishLine (%S)", this.startingLocation(), finishLine);
         } else {
             validator.validate(this.checkpoints.size() >= 2, "checkpoints must have at least 2 elements");
             List<BoundingBox> realCheckpoints = new ArrayList<>(this.checkpoints.size());
             for (int i = 0; i < this.checkpoints.size(); i++) {
-                BoundingBoxDTO checkpointDTO = this.checkpoints.get(i);
-                validator.notNull(checkpointDTO, "checkpoints[%d]", i);
-                BoundingBox checkpoint = checkpointDTO.toBoundingBox();
+                BoundingBox checkpoint = this.checkpoints.get(i);
+                validator.notNull(checkpoint, "checkpoints[%d]", i);
                 validator.validate(checkpoint.getVolume() >= 1.0, "checkpoints[%d]'s volume must be at least 1.0", i);
                 realCheckpoints.add(checkpoint);
             }
@@ -64,7 +61,6 @@ record FootRaceConfigDTO(
             }
         }
         if (spectatorArea != null) {
-            BoundingBox spectatorArea = this.spectatorArea.toBoundingBox();
             validator.validate(spectatorArea.getVolume() >= 1.0, "spectatorArea (%s) volume (%s) must be at least 1.0", spectatorArea, spectatorArea.getVolume());
         }
         validator.notNull(this.glassBarrier(), "glassBarrier");
@@ -101,7 +97,7 @@ record FootRaceConfigDTO(
         FootRaceConfig.FootRaceConfigBuilder builder = FootRaceConfig.builder()
                 .world(newWorld)
                 .startingLocation(this.startingLocation.toLocation(newWorld))
-                .glassBarrier(this.glassBarrier.toBoundingBox())
+                .glassBarrier(this.glassBarrier)
                 .completeLapScore(this.scores.completeLap)
                 .placementPoints(this.scores.placementPoints)
                 .detriment(this.scores.detriment)
@@ -110,16 +106,16 @@ record FootRaceConfigDTO(
                 .descriptionDuration(this.durations.description)
                 .gameOverDuration(this.durations.gameOver)
                 .preventInteractions(this.preventInteractions != null ? this.preventInteractions : Collections.emptyList())
-                .spectatorArea(this.spectatorArea != null ? this.spectatorArea.toBoundingBox() : null)
+                .spectatorArea(this.spectatorArea)
                 .description(this.description);
         if (this.checkpoints == null) {
             Preconditions.checkState(this.finishLine != null, "finishLine can't be null if checkpoints is null");
             builder
-                    .finishLine(this.finishLine.toBoundingBox())
+                    .finishLine(this.finishLine)
                     .useLegacy(true);
         } else {
             builder
-                    .checkpoints(new ArrayList<>(BoundingBoxDTO.toBoundingBoxes(this.checkpoints)))
+                    .checkpoints(new ArrayList<>(this.checkpoints))
                     .useLegacy(false);
         }
         return builder.build();
@@ -127,13 +123,13 @@ record FootRaceConfigDTO(
     
     public static FootRaceConfigDTO fromConfig(@NotNull FootRaceConfig config) {
         return new FootRaceConfigDTO(
-                Main.VALID_CONFIG_VERSIONS.get(Main.VALID_CONFIG_VERSIONS.size() - 1),
+                Main.VALID_CONFIG_VERSIONS.getLast(),
                 config.getWorld().getName(),
                 LocationDTO.from(config.getStartingLocation()),
-                config.getFinishLine() != null ? BoundingBoxDTO.from(config.getFinishLine()) : null,
-                BoundingBoxDTO.from(config.getCheckpoints()),
-                config.getSpectatorArea() != null ? BoundingBoxDTO.from(config.getSpectatorArea()) : null,
-                BoundingBoxDTO.from(config.getGlassBarrier()),
+                config.getFinishLine(),
+                config.getCheckpoints(),
+                config.getSpectatorArea(),
+                config.getGlassBarrier(),
                 config.getPreventInteractions(),
                 new Scores(
                         config.getCompleteLapScore(),
--- src/main/java/org/braekpo1nt/mctmanager/games/game/spleef/config/LayerDTO.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/spleef/config/LayerDTO.java
@@ -2,11 +2,11 @@ package org.braekpo1nt.mctmanager.games.game.spleef.config;
 
 import com.google.common.base.Preconditions;
 import org.braekpo1nt.mctmanager.config.dto.org.bukkit.NamespacedKeyDTO;
-import org.braekpo1nt.mctmanager.config.dto.org.bukkit.util.BoundingBoxDTO;
 import org.braekpo1nt.mctmanager.config.exceptions.ConfigInvalidException;
 import org.braekpo1nt.mctmanager.config.validation.Validatable;
 import org.braekpo1nt.mctmanager.config.validation.Validator;
 import org.bukkit.Bukkit;
+import org.bukkit.util.BoundingBox;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -17,7 +17,7 @@ import org.jetbrains.annotations.Nullable;
  * @param decayArea       the area in which to decay blocks for this layer. If this is null, the size of the structure and structureOrigin will be used as the area.
  */
 record LayerDTO(@Nullable NamespacedKeyDTO structure, Vector structureOrigin,
-                @Nullable BoundingBoxDTO decayArea) implements Validatable {
+                @Nullable BoundingBox decayArea) implements Validatable {
     @Override
     public void validate(@NotNull Validator validator) throws ConfigInvalidException {
         validator.validate(this.structure() != null, "layer.structure can't be null");
