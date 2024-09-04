c92b82304232f97667974deff84a1d1ab5cf84bc
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-09-04 10:00:35 -0400

    added arena datatype

---
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/farmrush/Arena.java
@@ -0,0 +1,49 @@
+package org.braekpo1nt.mctmanager.games.game.farmrush;
+
+import lombok.Builder;
+import lombok.Data;
+import org.bukkit.Location;
+import org.bukkit.block.BlockFace;
+import org.bukkit.util.BoundingBox;
+import org.bukkit.util.Vector;
+
+@Data
+@Builder
+public class Arena {
+    private BoundingBox bounds;
+    private BoundingBox barn;
+    private Location spawn;
+    private Location starterChest;
+    private Location delivery;
+    private BlockFace deliveryBlockFace;
+    
+    /**
+     * Create a new {@link Arena} from this arena using the given offset, where
+     * all Locations and BoundingBoxes are translated by the given offset. <br>
+     * This operation does not affect this {@link Arena}'s data.
+     * @param offset the offset to add to all the coordinates of this {@link Arena}
+     * @return a new {@link Arena} offset from the given 
+     */
+    public Arena offset(Vector offset) {
+        return Arena.builder()
+                .bounds(new BoundingBox(
+                        bounds.getMinX() + offset.getX(), 
+                        bounds.getMinY() + offset.getY(), 
+                        bounds.getMinZ() + offset.getZ(), 
+                        bounds.getMaxX() + offset.getX(), 
+                        bounds.getMaxY() + offset.getY(), 
+                        bounds.getMaxZ() + offset.getZ()))
+                .barn(new BoundingBox(
+                        barn.getMinX() + offset.getX(),
+                        barn.getMinY() + offset.getY(),
+                        barn.getMinZ() + offset.getZ(),
+                        barn.getMaxX() + offset.getX(),
+                        barn.getMaxY() + offset.getY(),
+                        barn.getMaxZ() + offset.getZ()))
+                .spawn(spawn.add(offset))
+                .starterChest(starterChest.add(offset))
+                .delivery(delivery.add(offset))
+                .deliveryBlockFace(deliveryBlockFace)
+                .build();
+    }
+}
