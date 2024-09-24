ac48ba938d1b5e870344f96ae1dafe769086fc0b
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-09-24 18:18:52 -0400

    modified key to use materials not itemstacks

--- src/main/java/org/braekpo1nt/mctmanager/config/dto/org/bukkit/inventory/recipes/ShapedRecipeDTO.java
+++ src/main/java/org/braekpo1nt/mctmanager/config/dto/org/bukkit/inventory/recipes/ShapedRecipeDTO.java
@@ -2,8 +2,7 @@ package org.braekpo1nt.mctmanager.config.dto.org.bukkit.inventory.recipes;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.braekpo1nt.mctmanager.config.dto.org.bukkit.inventory.ItemStackDTO;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
 
@@ -15,15 +14,15 @@ import java.util.Map;
 public class ShapedRecipeDTO extends RecipeDTO {
     
     protected List<String> pattern;
-    protected Map<Character, ItemStackDTO> key;
+    protected Map<Character, Material> key;
     
     @Override
     public Recipe toRecipe() {
         ShapedRecipe recipe = new ShapedRecipe(namespacedKey.toNamespacedKey(), result.toItemStack());
         recipe.shape(pattern.toArray(new String[0]));
-        for (Map.Entry<Character, ItemStackDTO> entry : key.entrySet()) {
+        for (Map.Entry<Character, Material> entry : key.entrySet()) {
             char keyChar = entry.getKey();
-            ItemStack item = entry.getValue().toItemStack();
+            Material item = entry.getValue();
             recipe.setIngredient(keyChar, item);
         }
         return recipe;
--- src/main/java/org/braekpo1nt/mctmanager/config/dto/org/bukkit/inventory/recipes/StonecuttingRecipeDTO.java
+++ src/main/java/org/braekpo1nt/mctmanager/config/dto/org/bukkit/inventory/recipes/StonecuttingRecipeDTO.java
@@ -3,17 +3,19 @@ package org.braekpo1nt.mctmanager.config.dto.org.bukkit.inventory.recipes;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.braekpo1nt.mctmanager.config.dto.org.bukkit.inventory.ItemStackDTO;
+import org.bukkit.Material;
 import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.StonecuttingRecipe;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class StonecuttingRecipeDTO extends RecipeDTO {
     
-    private ItemStackDTO ingredient;
+    private Material ingredient;
     
     @Override
     public Recipe toRecipe() {
-        return null;
+        return new StonecuttingRecipe(namespacedKey.toNamespacedKey(), result.toItemStack(), ingredient);
     }
     
 }
