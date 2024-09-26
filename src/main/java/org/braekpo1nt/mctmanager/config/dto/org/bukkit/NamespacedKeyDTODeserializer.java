7bd0d82f34ebc867c8be9b8d97e0ff7fdaa96bc2
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-09-26 15:47:33 -0400

    fixed broken tests

--- src/main/java/org/braekpo1nt/mctmanager/config/dto/org/bukkit/NamespacedKeyDTODeserializer.java
+++ src/main/java/org/braekpo1nt/mctmanager/config/dto/org/bukkit/NamespacedKeyDTODeserializer.java
@@ -1,6 +1,8 @@
 package org.braekpo1nt.mctmanager.config.dto.org.bukkit;
 
 import com.google.gson.*;
+import org.braekpo1nt.mctmanager.config.ConfigUtils;
+import org.bukkit.NamespacedKey;
 
 import java.lang.reflect.Type;
 
@@ -13,23 +15,43 @@ public class NamespacedKeyDTODeserializer implements JsonDeserializer<Namespaced
                 return null;
             }
             int indexOfColon = namespacedKeyStr.indexOf(':');
+            String namespace;
+            String key;
             if (indexOfColon < 0) {
-                return null;
+                namespace = NamespacedKey.MINECRAFT_NAMESPACE;
+                key = namespacedKeyStr;
+            } else {
+                namespace = namespacedKeyStr.substring(0, indexOfColon);
+                key = namespacedKeyStr.substring(indexOfColon+1);
+                if (!ConfigUtils.isValidNamespace(namespace)) {
+                    throw new JsonParseException(String.format("namespace must be [a-z0-9._-]: %s", namespace));
+                }
             }
-            String namespace = namespacedKeyStr.substring(0, indexOfColon);
-            String key = namespacedKeyStr.substring(indexOfColon+1);
+            if (!ConfigUtils.isValidKey(key)) {
+                throw new JsonParseException(String.format("key must be [a-z0-9/._-]: %s", key));
+            }
             return new NamespacedKeyDTO(namespace, key);
         } else if (json.isJsonObject()) {
             JsonObject jsonObject = json.getAsJsonObject();
-            JsonElement namespace = jsonObject.get("namespace");
-            JsonElement key = jsonObject.get("key");
-            if (namespace == null) {
+            JsonElement namespaceElement = jsonObject.get("namespace");
+            JsonElement keyElement = jsonObject.get("key");
+            String namespace;
+            if (namespaceElement == null) {
+                namespace = "minecraft";
+            } else {
+                namespace = namespaceElement.getAsString();
+                if (!ConfigUtils.isValidNamespace(namespace)) {
+                    throw new JsonParseException(String.format("namespace must be [a-z0-9._-]: %s", namespace));
+                }
+            }
+            if (keyElement == null) {
                 return null;
             }
-            if (key == null) {
-                return null;
+            String key = keyElement.getAsString();
+            if (!ConfigUtils.isValidKey(key)) {
+                throw new JsonParseException(String.format("key must be [a-z0-9/._-]: %s", key));
             }
-            return new NamespacedKeyDTO(namespace.getAsString(), key.getAsString());
+            return new NamespacedKeyDTO(namespace, key);
         }
         return null;
     }
