1aaf14ac033b93335d66dcb8eb9178e6ef8082d4
Braekpo1nt <braekpo1ntofficial@gmail.com>
2024-11-04 14:37:06 -0500

    includes /top message

--- src/main/java/org/braekpo1nt/mctmanager/games/game/farmrush/states/ActiveState.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/farmrush/states/ActiveState.java
@@ -1,6 +1,7 @@
 package org.braekpo1nt.mctmanager.games.game.farmrush.states;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.title.Title;
@@ -15,10 +16,17 @@ import org.jetbrains.annotations.NotNull;
 
 public class ActiveState extends GameplayState {
     
-    protected final Timer gameTimer;
+    private final Timer gameTimer;
+    private final Component slashTopMessage;
     
     public ActiveState(@NotNull FarmRushGame context) {
         super(context);
+        TopCommand.setEnabled(true);
+        slashTopMessage = Component.empty()
+                .append(Component.text("Use "))
+                .append(Component.text("/top")
+                        .decorate(TextDecoration.UNDERLINED)
+                        .clickEvent(ClickEvent.suggestCommand("/top")));
         gameTimer = context.getTimerManager().start(Timer.builder()
                 .duration(context.getConfig().getGameDuration())
                 .withSidebar(context.getSidebar(), "timer")
@@ -26,20 +34,20 @@ public class ActiveState extends GameplayState {
                 .completionSeconds(context.getConfig().getGracePeriodDuration())
                 .onCompletion(() -> {
                     Component timeLeft = TimeStringUtils.getTimeComponent(context.getConfig().getGracePeriodDuration());
-                    Component warning = Component.empty()
-                            .append(timeLeft)
-                            .append(Component.text(" left!"))
-                            .color(NamedTextColor.RED);
-                    Title warningTitle = UIUtils.defaultTitle(Component.empty(), warning);
-                    context.messageAllParticipants(warning);
-                    context.titleAllParticipants(warningTitle);
+                    context.titleAllParticipants(UIUtils.defaultTitle(
+                            Component.empty(),
+                            Component.empty()
+                                    .append(timeLeft)
+                                    .append(Component.text(" left!"))
+                                    .color(NamedTextColor.RED)));
+                    context.messageAllParticipants(slashTopMessage);
                     context.setState(new GracePeriodState(context));
                 })
                 .build());
         for (FarmRushGame.Participant participant : context.getParticipants().values()) {
             participant.getPlayer().setGameMode(GameMode.SURVIVAL);
         }
-        TopCommand.setEnabled(true);
+        
         for (FarmRushGame.Team team : context.getTeams().values()) {
             team.getArena().openBarnDoor();
         }
@@ -72,18 +80,18 @@ public class ActiveState extends GameplayState {
                 .append(Component.text((int) (context.getConfig().getMaxScore() * gameManager.matchProgressPointMultiplier()))
                         .color(NamedTextColor.GOLD)
                         .decorate(TextDecoration.BOLD))
-                .append(Component.text(" points first!"))
+                .append(Component.text(" points first! "))
+                .append(slashTopMessage)
         );
         Component timeLeft = TimeStringUtils.getTimeComponent(context.getConfig().getGracePeriodDuration());
-        Component warning = Component.empty()
-                .append(timeLeft)
-                .append(Component.text(" left!"))
-                .color(NamedTextColor.RED);
-        Title warningTitle = UIUtils.defaultTitle(Component.empty()
-                .append(winingTeam.getDisplayName())
-                .append(Component.text("wins!")), warning);
-        context.messageAllParticipants(warning);
-        context.titleAllParticipants(warningTitle);
+        context.titleAllParticipants(UIUtils.defaultTitle(
+                Component.empty()
+                        .append(winingTeam.getDisplayName())
+                        .append(Component.text("wins!")),
+                Component.empty()
+                        .append(timeLeft)
+                        .append(Component.text(" left!"))
+                        .color(NamedTextColor.RED)));
         context.setState(new GracePeriodState(context));
     }
     
