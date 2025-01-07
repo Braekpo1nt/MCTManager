210b14dc70826ec6336dfa11b51e285785ee26f5
Braekpo1nt <braekpo1ntofficial@gmail.com>
2025-01-07 14:22:44 -0500

    added bulk team score update to TabList

--- src/main/java/org/braekpo1nt/mctmanager/ui/tablist/TabList.java
+++ src/main/java/org/braekpo1nt/mctmanager/ui/tablist/TabList.java
@@ -283,19 +283,17 @@ public class TabList {
     
     /**
      * Set the scores of the given teams
-     * @param teamIds the teamIds to update the scores of. Any invalid teamIds not in this TabList will be ignored
-     *                and a log message will announce the error.
-     * @param scores the scores to update the teams to. Must be the same length as the teamIds list.
+     * @param teamIdsToScores the teamIds to update mapped to their new scores. 
+     *                        (Any teamIds not in this TabList will be ignored
+     *                        and a log message will announce the error.)
      */
-    public void setScores(@NotNull List<@NotNull String> teamIds, @NotNull List<@NotNull Integer> scores) {
-        if (teamIds.size() != scores.size()) {
-            logUIError("Size of teamIds (%d) doesn't match size of scores (%d)", teamIds.size(), scores.size());
-            return;
-        }
-        for (int i = 0; i < teamIds.size(); i++) {
-            TeamData teamData = getTeamData(teamIds.get(i));
+    public void setScores(@NotNull Map<@NotNull String, @NotNull Integer> teamIdsToScores) {
+        for (Map.Entry<String, Integer> entry : teamIdsToScores.entrySet()) {
+            String teamId = entry.getKey();
+            int score = entry.getValue();
+            TeamData teamData = getTeamData(teamId);
             if (teamData != null) {
-                teamData.setScore(scores.get(i));
+                teamData.setScore(score);
             }
         }
         update();
