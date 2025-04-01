2821175fa0946f2fd84a3ac6b3bc91b8a999e72f
Braekpo1nt <braekpo1ntofficial@gmail.com>
2025-04-01 15:14:56 -0400

    added INitialState

---
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/survivalgames/states/InitialState.java
@@ -0,0 +1,77 @@
+package org.braekpo1nt.mctmanager.games.game.survivalgames.states;
+
+import org.braekpo1nt.mctmanager.games.game.survivalgames.SurvivalGamesParticipant;
+import org.braekpo1nt.mctmanager.games.game.survivalgames.SurvivalGamesTeam;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class InitialState implements SurvivalGamesState {
+    @Override
+    public void onParticipantDeath(PlayerDeathEvent event) {
+        
+    }
+    
+    @Override
+    public void onPlayerDamage(EntityDamageEvent event) {
+        
+    }
+    
+    @Override
+    public void cleanup() {
+        
+    }
+    
+    @Override
+    public void onTeamRejoin(SurvivalGamesTeam team) {
+        
+    }
+    
+    @Override
+    public void onNewTeamJoin(SurvivalGamesTeam team) {
+        
+    }
+    
+    @Override
+    public void onParticipantRejoin(SurvivalGamesParticipant participant, SurvivalGamesTeam team) {
+        
+    }
+    
+    @Override
+    public void onNewParticipantJoin(SurvivalGamesParticipant participant, SurvivalGamesTeam team) {
+        
+    }
+    
+    @Override
+    public void onParticipantQuit(SurvivalGamesParticipant participant, SurvivalGamesTeam team) {
+        
+    }
+    
+    @Override
+    public void onTeamQuit(SurvivalGamesTeam team) {
+        
+    }
+    
+    @Override
+    public void onParticipantMove(@NotNull PlayerMoveEvent event, @NotNull SurvivalGamesParticipant participant) {
+        
+    }
+    
+    @Override
+    public void onParticipantTeleport(@NotNull PlayerTeleportEvent event, @NotNull SurvivalGamesParticipant participant) {
+        
+    }
+    
+    @Override
+    public void onParticipantInteract(@NotNull PlayerInteractEvent event, @NotNull SurvivalGamesParticipant participant) {
+        
+    }
+    
+    @Override
+    public void onParticipantDamage(@NotNull EntityDamageEvent event, @NotNull SurvivalGamesParticipant participant) {
+        
+    }
+}
