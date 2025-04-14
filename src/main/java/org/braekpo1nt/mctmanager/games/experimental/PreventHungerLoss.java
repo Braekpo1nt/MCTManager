8fb81f6430d054dd1890e84aaca63f114ffa0069
Braekpo1nt <braekpo1ntofficial@gmail.com>
2025-04-14 13:24:31 -0400

    moved GameData to GameBase

--- src/main/java/org/braekpo1nt/mctmanager/games/experimental/PreventHungerLoss.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/experimental/PreventHungerLoss.java
@@ -1,6 +1,6 @@
 package org.braekpo1nt.mctmanager.games.experimental;
 
-import org.braekpo1nt.mctmanager.participant.Participant;
+import org.braekpo1nt.mctmanager.participant.ParticipantData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.jetbrains.annotations.NotNull;
@@ -9,14 +9,15 @@ import org.jetbrains.annotations.NotNull;
  * Prevents participants from losing hunger. 
  * @param <P>
  */
-public class PreventHungerLoss<P extends Participant> extends GameListener<P> {
-    public PreventHungerLoss(@NotNull GameData<P> gameData) {
-        super(gameData);
+public class PreventHungerLoss<P extends ParticipantData> extends GameListener<P> {
+    
+    public PreventHungerLoss(@NotNull GameBase<P, ?, ?, ?, ?> context) {
+        super(context);
     }
     
     @EventHandler
     public void onFoodLevelChange(FoodLevelChangeEvent event) {
-        P participant = gameData.getParticipant(event.getEntity().getUniqueId());
+        P participant = context.getParticipant(event.getEntity().getUniqueId());
         if (participant == null) {
             return;
         }
