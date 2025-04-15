e3e28d7035869acf62d5e5bd3cb37a12c22853d9
Braekpo1nt <braekpo1ntofficial@gmail.com>
2025-04-15 13:35:50 -0400

    fixed players turning to adventure on game end

--- src/main/java/org/braekpo1nt/mctmanager/games/game/colossalcombat/states/GameOverState.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/colossalcombat/states/GameOverState.java
@@ -1,8 +1,13 @@
 package org.braekpo1nt.mctmanager.games.game.colossalcombat.states;
 
 import net.kyori.adventure.text.Component;
+import org.braekpo1nt.mctmanager.games.experimental.Affiliation;
 import org.braekpo1nt.mctmanager.games.game.colossalcombat.ColossalCombatGame;
+import org.braekpo1nt.mctmanager.games.game.colossalcombat.ColossalParticipant;
+import org.braekpo1nt.mctmanager.games.utils.ParticipantInitializer;
 import org.braekpo1nt.mctmanager.ui.timer.Timer;
+import org.bukkit.GameMode;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class GameOverState extends ColossalCombatStateBase {
@@ -19,5 +24,18 @@ public class GameOverState extends ColossalCombatStateBase {
                     context.stop();
                 })
                 .build());
+    }
+    
+    @Override
+    public void onParticipantRespawn(PlayerRespawnEvent event, ColossalParticipant participant) {
+        super.onParticipantRespawn(event, participant);
+        if (participant.getAffiliation() == Affiliation.SPECTATOR) {
+            return;
+        }
+        event.setRespawnLocation(participant.getLocation());
+        participant.setGameMode(GameMode.SPECTATOR);
+        ParticipantInitializer.resetHealthAndHunger(participant);
+        ParticipantInitializer.clearStatusEffects(participant);
+        ParticipantInitializer.clearInventory(participant);
     }
 }
