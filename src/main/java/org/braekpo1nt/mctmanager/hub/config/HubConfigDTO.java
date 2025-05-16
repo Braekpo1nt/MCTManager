e7b61a24eeeb93da6a4c066fb6dfe906f026c61e
Braekpo1nt <braekpo1ntofficial@gmail.com>
2025-05-16 16:21:37 -0400

    changed record to class

--- src/main/java/org/braekpo1nt/mctmanager/hub/config/HubConfigDTO.java
+++ src/main/java/org/braekpo1nt/mctmanager/hub/config/HubConfigDTO.java
@@ -19,37 +19,41 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-record HubConfigDTO(
-        String version,
-        String world,
-        LocationDTO spawn,
-        LocationDTO podium,
-        LocationDTO podiumObservation,
-        PracticeDTO practice,
-        List<LeaderboardDTO> leaderboards,
-        List<Material> preventInteractions,
-        double yLimit,
-        Durations durations) implements Validatable {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class HubConfigDTO implements Validatable {
+    
+    private String version;
+    private String world;
+    private LocationDTO spawn;
+    private LocationDTO podium;
+    private LocationDTO podiumObservation;
+    private PracticeDTO practice;
+    private List<LeaderboardDTO> leaderboards;
+    private List<Material> preventInteractions;
+    private double yLimit;
+    private Durations durations;
     
     @Override
     public void validate(@NotNull Validator validator) {
-        validator.notNull(this.version(), "version");
-        validator.validate(Main.VALID_CONFIG_VERSIONS.contains(this.version()), "invalid config version (%s)", this.version());
+        validator.notNull(this.version, "version");
+        validator.validate(Main.VALID_CONFIG_VERSIONS.contains(this.version), "invalid config version (%s)", this.version);
         validator.notNull(this.world, "world");
-        validator.notNull(Bukkit.getWorld(this.world()), "Could not find world \"%s\"", this.world());
-        validator.notNull(this.spawn(), "spawn");
-        validator.notNull(this.podium(), "podium");
+        validator.notNull(Bukkit.getWorld(this.world), "Could not find world \"%s\"", this.world);
+        validator.notNull(this.spawn, "spawn");
+        validator.notNull(this.podium, "podium");
         validator.notNull(practice, "practice");
         practice.validate(validator.path("practice"));
         if (leaderboards != null) {
             validator.validateList(leaderboards, "leaderboards");
         }
-        validator.validate(this.yLimit() < this.spawn().getY(), "yLimit (%s) must be less than spawn.y (%s)", this.yLimit(), this.spawn().getY());
-        validator.validate(this.yLimit() < this.podium().getY(), "yLimit (%s) must be less than podium.y (%s)", this.yLimit(), this.podium().getY());
-        validator.validate(this.yLimit() < this.podiumObservation().getY(), "yLimit (%s) must be less than podiumObservation.y (%s)", this.yLimit(), this.podiumObservation().getY());
-        validator.validate(this.yLimit() < this.podiumObservation().getY(), "yLimit (%s) must be less than podiumObservation.y (%s)", this.yLimit(), this.podiumObservation().getY());
-        validator.notNull(this.durations(), "durations");
-        validator.validate(this.durations().tpToHub() > 0, "durations.tpToHub must be greater than 0");
+        validator.validate(this.yLimit < this.spawn.getY(), "yLimit (%s) must be less than spawn.y (%s)", this.yLimit, this.spawn.getY());
+        validator.validate(this.yLimit < this.podium.getY(), "yLimit (%s) must be less than podium.y (%s)", this.yLimit, this.podium.getY());
+        validator.validate(this.yLimit < this.podiumObservation.getY(), "yLimit (%s) must be less than podiumObservation.y (%s)", this.yLimit, this.podiumObservation.getY());
+        validator.validate(this.yLimit < this.podiumObservation.getY(), "yLimit (%s) must be less than podiumObservation.y (%s)", this.yLimit, this.podiumObservation.getY());
+        validator.notNull(this.durations, "durations");
+        validator.validate(this.durations.tpToHub() > 0, "durations.tpToHub must be greater than 0");
         
     }
     
