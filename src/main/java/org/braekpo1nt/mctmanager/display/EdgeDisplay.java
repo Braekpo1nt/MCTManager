e01cd27b8244204ee90ee3486d7994719eb6082f
Braekpo1nt <braekpo1ntofficial@gmail.com>
2025-06-03 18:34:33 -0400

    Squashed commit of the following:
    commit 2d6f315748b5279363c6477c22b83e80c33c2bb0
    Author: Braekpo1nt <braekpo1ntofficial@gmail.com>
    Date:   Tue Jun 3 18:34:14 2025 -0400
    
        started on edge dispaly

--- src/main/java/org/braekpo1nt/mctmanager/display/EdgeDisplay.java
+++ src/main/java/org/braekpo1nt/mctmanager/display/EdgeDisplay.java
@@ -2,23 +2,44 @@ package org.braekpo1nt.mctmanager.display;
 
 import org.braekpo1nt.mctmanager.display.geometry.Edge;
 import org.bukkit.Color;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.entity.BlockDisplay;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class EdgeDisplay implements Display {
     
+    private @NotNull Edge edge;
+    private @NotNull Color glowColor;
+    private @Nullable BlockDisplay blockDisplay;
+    
+    private boolean showing;
+    
+    public EdgeDisplay(@NotNull Edge edge, @NotNull Color glowColor, @NotNull Material material) {
+        this.edge = edge;
+        this.glowColor = glowColor;
+        this.showing = false;
+    }
+    
     public EdgeDisplay(@NotNull Edge edge, @NotNull Color glowColor) {
-        // TODO: implement this
-        throw new UnsupportedOperationException("not yet implemented");
+        this(edge, glowColor, Material.RED_WOOL);
     }
     
     @Override
     public void show(@NotNull World world) {
+        if (showing) {
+            return;
+        }
+        showing = true;
         
     }
     
     @Override
     public void hide() {
-        
+        if (!showing) {
+            return;
+        }
+        showing = false;
     }
 }
