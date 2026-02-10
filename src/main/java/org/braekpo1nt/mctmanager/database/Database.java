43a732ce84680533dd3341e6e815428a1988de98
Braekpo1nt <braekpo1ntofficial@gmail.com>
2026-02-10 17:26:36 -0500

    replaced all daos in Database

--- src/main/java/org/braekpo1nt/mctmanager/database/Database.java
+++ src/main/java/org/braekpo1nt/mctmanager/database/Database.java
@@ -5,13 +5,25 @@ import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import lombok.Getter;
+import org.braekpo1nt.mctmanager.database.entities.AllPlayersEntity;
 import org.braekpo1nt.mctmanager.database.entities.EventInfo;
+import org.braekpo1nt.mctmanager.database.entities.EventParticipantStanding;
+import org.braekpo1nt.mctmanager.database.entities.EventTeamStanding;
 import org.braekpo1nt.mctmanager.database.entities.FinalPersonalScore;
 import org.braekpo1nt.mctmanager.database.entities.FinalTeamScore;
 import org.braekpo1nt.mctmanager.database.entities.GameSession;
 import org.braekpo1nt.mctmanager.database.entities.InstantPersonalScore;
 import org.braekpo1nt.mctmanager.database.entities.InstantTeamScore;
 import org.braekpo1nt.mctmanager.database.entities.ParticipantData;
+import org.braekpo1nt.mctmanager.database.entities.PlayerMetadata;
+import org.braekpo1nt.mctmanager.database.entities.ScoreEvent;
+import org.braekpo1nt.mctmanager.database.entities.SystemState;
+import org.braekpo1nt.mctmanager.database.entities.participants.EventParticipantEntity;
+import org.braekpo1nt.mctmanager.database.entities.participants.MaintenanceParticipantEntity;
+import org.braekpo1nt.mctmanager.database.entities.participants.PracticeParticipantEntity;
+import org.braekpo1nt.mctmanager.database.entities.teams.EventTeam;
+import org.braekpo1nt.mctmanager.database.entities.teams.MaintenanceTeam;
+import org.braekpo1nt.mctmanager.database.entities.teams.PracticeTeam;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.SQLException;
@@ -20,15 +32,47 @@ import java.sql.SQLException;
 public class Database {
     
     // ScoreService
-    private final @NotNull Dao<InstantPersonalScore, Integer> instantPersonalScoreDao;
-    private final @NotNull Dao<InstantTeamScore, Integer> instantTeamScoreDao;
-    private final @NotNull Dao<FinalPersonalScore, Integer> finalPersonalScoreDao;
-    private final @NotNull Dao<FinalTeamScore, Integer> finalTeamScoreDao;
-    private final @NotNull Dao<GameSession, Integer> gameSessionDao;
-    private final @NotNull Dao<ParticipantData, String> participantDataDao;
+    private @Deprecated Dao<InstantPersonalScore, Integer> instantPersonalScoreDao;
+    private @Deprecated Dao<InstantTeamScore, Integer> instantTeamScoreDao;
+    private @Deprecated Dao<FinalPersonalScore, Integer> finalPersonalScoreDao;
+    private @Deprecated Dao<FinalTeamScore, Integer> finalTeamScoreDao;
+    private @Deprecated Dao<ParticipantData, String> participantDataDao;
     
-    // EventService
+    private final @NotNull Dao<AllPlayersEntity, String> allPlayersDao;
     private final @NotNull Dao<EventInfo, String> eventInfoDao;
+    private final @NotNull Dao<SystemState, Integer> systemStateDao;
+    private final @NotNull Dao<MaintenanceTeam, String> maintenanceTeamsDao;
+    private final @NotNull Dao<PracticeTeam, String> practiceTeamsDao;
+    private final @NotNull Dao<EventTeam, Integer> eventTeamsDao;
+    private final @NotNull Dao<MaintenanceParticipantEntity, String> maintenanceParticipantsDao;
+    private final @NotNull Dao<PracticeParticipantEntity, String> practiceParticipantsDao;
+    private final @NotNull Dao<EventParticipantEntity, Integer> eventParticipantsDao;
+    private final @NotNull Dao<GameSession, Integer> gameSessionDao;
+    private final @NotNull Dao<ScoreEvent, Integer> scoreEventsDao;
+    private final @NotNull Dao<EventTeamStanding, Integer> eventTeamStandingsDao;
+    private final @NotNull Dao<EventParticipantStanding, Integer> eventParticipantStandingsDao;
+    private final @NotNull Dao<PlayerMetadata, String> playerMetadataDao;
+    
+    private Database(@NotNull ConnectionSource connectionSource) throws SQLException {
+        // flyway creates the tables, no need for TableUtils:
+//        TableUtils.createTableIfNotExists(connectionSource, EventInfo.class);
+        
+        // Create the DAOs
+        this.allPlayersDao = DaoManager.createDao(connectionSource, AllPlayersEntity.class);
+        this.eventInfoDao = DaoManager.createDao(connectionSource, EventInfo.class);
+        this.systemStateDao = DaoManager.createDao(connectionSource, SystemState.class);
+        this.maintenanceTeamsDao = DaoManager.createDao(connectionSource, MaintenanceTeam.class);
+        this.practiceTeamsDao = DaoManager.createDao(connectionSource, PracticeTeam.class);
+        this.eventTeamsDao = DaoManager.createDao(connectionSource, EventTeam.class);
+        this.maintenanceParticipantsDao = DaoManager.createDao(connectionSource, MaintenanceParticipantEntity.class);
+        this.practiceParticipantsDao = DaoManager.createDao(connectionSource, PracticeParticipantEntity.class);
+        this.eventParticipantsDao = DaoManager.createDao(connectionSource, EventParticipantEntity.class);
+        this.gameSessionDao = DaoManager.createDao(connectionSource, GameSession.class);
+        this.scoreEventsDao = DaoManager.createDao(connectionSource, ScoreEvent.class);
+        this.eventTeamStandingsDao = DaoManager.createDao(connectionSource, EventTeamStanding.class);
+        this.eventParticipantStandingsDao = DaoManager.createDao(connectionSource, EventParticipantStanding.class);
+        this.playerMetadataDao = DaoManager.createDao(connectionSource, PlayerMetadata.class);
+    }
     
     public Database(
             String host,
@@ -41,22 +85,5 @@ public class Database {
     
     public Database(String sqlitePath) throws SQLException {
         this(new JdbcConnectionSource("jdbc:sqlite:" + sqlitePath));
-    }
-    
-    private Database(@NotNull ConnectionSource connectionSource) throws SQLException {
-        // flyway creates the tables, no need for TableUtils:
-//        TableUtils.createTableIfNotExists(connectionSource, EventInfo.class);
-        
-        // Create the DAOs
-        // ScoreService
-        this.instantPersonalScoreDao = DaoManager.createDao(connectionSource, InstantPersonalScore.class);
-        this.instantTeamScoreDao = DaoManager.createDao(connectionSource, InstantTeamScore.class);
-        this.gameSessionDao = DaoManager.createDao(connectionSource, GameSession.class);
-        this.finalPersonalScoreDao = DaoManager.createDao(connectionSource, FinalPersonalScore.class);
-        this.finalTeamScoreDao = DaoManager.createDao(connectionSource, FinalTeamScore.class);
-        this.participantDataDao = DaoManager.createDao(connectionSource, ParticipantData.class);
-        
-        // EventService
-        this.eventInfoDao = DaoManager.createDao(connectionSource, EventInfo.class);
     }
 }
