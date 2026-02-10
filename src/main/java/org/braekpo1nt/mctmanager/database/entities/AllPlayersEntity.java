3a3a1cce5af2f69bc42002bdac370ab4165cc6e8
Braekpo1nt <braekpo1ntofficial@gmail.com>
2026-02-10 10:10:26 -0500

    created AllPlayersEntity

--- src/main/java/org/braekpo1nt/mctmanager/database/entities/AllPlayersEntity.java
+++ src/main/java/org/braekpo1nt/mctmanager/database/entities/AllPlayersEntity.java
@@ -1,12 +1,22 @@
 package org.braekpo1nt.mctmanager.database.entities;
 
+import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @DatabaseTable(tableName = "all_players")
 @NoArgsConstructor
 @Data
 public class AllPlayersEntity {
-    
+    @DatabaseField(id = true)
+    private String uuid;
+    @DatabaseField(canBeNull = false)
+    private String ign;
+    @DatabaseField(columnName = "discord_username")
+    private String discordUsername;
+    @DatabaseField(columnName = "first_seen_at")
+    private Date firstSeenAt;
 }
