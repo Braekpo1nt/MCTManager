029c63108fbcaea79edef94f39fa431ba2e51757
Braekpo1nt <braekpo1ntofficial@gmail.com>
2026-03-20 08:57:04 -0400

    made load toggle simpler

--- src/main/java/org/braekpo1nt/mctmanager/commands/mct/mode/ModeCommand.java
+++ src/main/java/org/braekpo1nt/mctmanager/commands/mct/mode/ModeCommand.java
@@ -1,12 +1,11 @@
 package org.braekpo1nt.mctmanager.commands.mct.mode;
 
-import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
-import org.braekpo1nt.mctmanager.commands.manager.brigadier.permissioned.Permissioned;
 import org.braekpo1nt.mctmanager.commands.argumenttypes.EnumArgumentType;
 import org.braekpo1nt.mctmanager.commands.manager.brigadier.BrigadierAdapters;
 import org.braekpo1nt.mctmanager.commands.manager.brigadier.BrigadierSubCommand;
+import org.braekpo1nt.mctmanager.commands.manager.brigadier.permissioned.Permissioned;
 import org.braekpo1nt.mctmanager.commands.manager.commandresult.CommandResult;
 import org.braekpo1nt.mctmanager.games.gamemanager.GameManager;
 import org.braekpo1nt.mctmanager.games.gamemanager.Mode;
@@ -25,7 +24,7 @@ public class ModeCommand implements BrigadierSubCommand {
         return Permissioned.literal("mode")
                 .then(Permissioned.argument("mode", new EnumArgumentType<>(Mode.class, Mode.values()))
                         .executes(BrigadierAdapters.wraps(this::executeMode))
-                        .then(Permissioned.argument("load", BoolArgumentType.bool())
+                        .then(Permissioned.literal("load")
                                 .executes(BrigadierAdapters.wraps(this::executeModeLoad))
                         )
                 )
@@ -39,7 +38,6 @@ public class ModeCommand implements BrigadierSubCommand {
     
     private @NotNull CommandResult executeModeLoad(@NotNull CommandContext<CommandSourceStack> ctx) {
         Mode mode = ctx.getArgument("mode", Mode.class);
-        boolean load = ctx.getArgument("load", Boolean.class);
-        return gameManager.switchMode(mode, load);
+        return gameManager.switchMode(mode, true);
     }
 }
