785b4c2e7c6dcefab912383124064f2fe25acf90
Braekpo1nt <94723887+Braekpo1nt@users.noreply.github.com>
2026-07-20 15:13:47 -0400

    850 team points for foot race (#866)
    * Created 4 new config variables
    Implemented multiple methods in ActiveState of FootRace
    Points should now be awarded for full team lap completion
    Should announce in chat what placement the team completed the lap in the team's color.
    
    * Fixed announceTeamLapCompletion
    
    * Resolved all comments and made appropriate changes
    
    * Shadow build and completed final touches
    
    * Add equals sign
    
    * Teams no longer see +0 if team points <= 0 for completing lap/race
    
    * Updated UI/UX for completion
    
    ---------
    
    Co-authored-by: SgtShotgun <gageedwards06@outlook.com>

--- src/main/java/org/braekpo1nt/mctmanager/games/game/footrace/FootRaceTeam.java
+++ src/main/java/org/braekpo1nt/mctmanager/games/game/footrace/FootRaceTeam.java
@@ -1,21 +1,35 @@
 package org.braekpo1nt.mctmanager.games.game.footrace;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.braekpo1nt.mctmanager.participant.QuitDataBase;
 import org.braekpo1nt.mctmanager.participant.ScoredTeamData;
 import org.braekpo1nt.mctmanager.participant.Team;
 
 public class FootRaceTeam extends ScoredTeamData<FootRaceParticipant> {
+    /**
+     * The lowest lap number across this team's participants
+     */
+    @Getter @Setter private int minimumLap;
+    
     public FootRaceTeam(Team team, int score) {
         super(team, score);
+        this.minimumLap = 1;
+    }
+    
+    public FootRaceTeam(Team team, FootRaceTeam.QuitData quitData) {
+        super(team, quitData.getScore());
+        this.minimumLap = quitData.getMinimumLap();
     }
     
     @Data
     public static class QuitData implements QuitDataBase {
         private final int score;
+        private final int minimumLap;
     }
     
     public QuitData getQuitData() {
-        return new QuitData(getScore());
+        return new QuitData(getScore(), minimumLap);
     }
 }
